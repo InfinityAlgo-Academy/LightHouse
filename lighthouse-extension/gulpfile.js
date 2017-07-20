6f53ab9ff0b2e78e9a55691bdccc0c7b88f0cc76
Paul Irish <paul.irish@gmail.com>
2017-07-20 14:02:24 -0700

    gulpfile. add commented out lines for sourcemap generation.

--- lighthouse-extension/gulpfile.js
+++ lighthouse-extension/gulpfile.js
@@ -101,7 +101,7 @@ gulp.task('browserify-lighthouse', () => {
     'app/src/lighthouse-background.js'
   ], {read: false})
     .pipe(tap(file => {
-      let bundle = browserify(file.path); // , {debug: true})
+      let bundle = browserify(file.path); // , {debug: true}); // for sourcemaps
       bundle = applyBrowserifyTransforms(bundle);
 
       // lighthouse-background will need some additional transforms, ignores and requires…
@@ -161,6 +161,7 @@ gulp.task('compilejs', () => {
     retainLines: true, // Keep things on the same line (looks wonky but helps with stacktraces)
     comments: false, // Don't output comments
     shouldPrintComment: _ => false, // Don't include @license or @preserve comments either
+    // sourceMaps: 'both'
   };
 
   return gulp.src(['dist/scripts/lighthouse-background.js'])
