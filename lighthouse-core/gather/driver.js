d7ffec1b78c12eb2437939413accee0b613b30f9
Paul Irish <paul.irish@gmail.com>
2017-08-04 10:56:07 -0700

    driver: add compat comment on PerfObserver GC bug

--- lighthouse-core/gather/driver.js
+++ lighthouse-core/gather/driver.js
@@ -1118,6 +1118,8 @@ function registerPerformanceObserverInPage() {
   // HACK: A PerformanceObserver will be GC'd if there are no more references to it, so attach it to
   // window to ensure we still receive longtask notifications. See https://crbug.com/742530.
   // For an example test of this behavior see https://gist.github.com/patrickhulce/69d8bed1807e762218994b121d06fea6.
+  //   FIXME COMPAT: This hack isn't neccessary as of Chrome 62.0.3176.0
+  //   https://bugs.chromium.org/p/chromium/issues/detail?id=742530#c7
   window.____lhPerformanceObserver = observer;
 }
 
