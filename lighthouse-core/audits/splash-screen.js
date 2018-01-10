b13efadb58c6b5ed68b1e83272990b017a234b38
Kayce Basques <kaycebasques@gmail.com>
2018-01-09 18:23:22 -0800

    [docs] link to splash screen reference (#4129)

--- lighthouse-core/audits/splash-screen.js
+++ lighthouse-core/audits/splash-screen.js
@@ -29,7 +29,9 @@ class SplashScreen extends MultiCheckAudit {
       name: 'splash-screen',
       description: 'Configured for a custom splash screen',
       failureDescription: 'Is not configured for a custom splash screen',
-      helpText: 'A default splash screen will be constructed for your app, but satisfying these requirements guarantee a high-quality [splash screen](https://developers.google.com/web/updates/2015/10/splashscreen) that transitions the user from tapping the home screen icon to your app\'s first paint',
+      helpText: 'A themed splash screen ensures a high-quality experience when ' +
+          'users launch your app from their homescreens. [Learn ' +
+          'more](https://developers.google.com/web/tools/lighthouse/audits/custom-splash-screen).',
       requiredArtifacts: ['Manifest'],
     };
   }
