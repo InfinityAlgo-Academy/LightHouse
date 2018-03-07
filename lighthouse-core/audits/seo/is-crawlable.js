eb982c85199039ae4a44f90bfbc39222d6843bc6
Kayce Basques <kaycebasques@gmail.com>
2018-03-07 10:11:00 -0800

    report(seo): update link to the is-crawlable/indexing reference (#4678)

--- lighthouse-core/audits/seo/is-crawlable.js
+++ lighthouse-core/audits/seo/is-crawlable.js
@@ -65,8 +65,9 @@ class IsCrawlable extends Audit {
       name: 'is-crawlable',
       description: 'Page isn’t blocked from indexing',
       failureDescription: 'Page is blocked from indexing',
-      helpText: 'The "Robots" directives tell crawlers how your content should be indexed. ' +
-      '[Learn more](https://developers.google.com/search/reference/robots_meta_tag).',
+      helpText: 'Search engines are unable to include your pages in search results ' +
+          'if they don\'t have permission to crawl them. [Learn ' +
+          'more](https://developers.google.com/lighthouse/audits/indexing).',
       requiredArtifacts: ['MetaRobots', 'RobotsTxt'],
     };
   }
