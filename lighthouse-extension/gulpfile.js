971f3a413fecd51a7514db7497fc75a8729146bd
Paul Irish <paul.irish@gmail.com>
2018-03-15 13:31:53 -0700

    revert 91ae3070:  core: make url-shim extend native URL module; add type checking (#4712)

--- lighthouse-extension/gulpfile.js
+++ lighthouse-extension/gulpfile.js
@@ -121,6 +121,7 @@ gulp.task('browserify-lighthouse', () => {
       // to the modified version internal to Lighthouse.
       bundle.transform('./dtm-transform.js', {global: true})
       .ignore('source-map')
+      .ignore('whatwg-url')
       .ignore('url')
       .ignore('debug/node')
       .ignore('raven')
