202d04b3359526cbe49531f03c61258002da073c
Paul Irish <paul.irish@gmail.com>
2018-03-15 12:14:27 -0700

    core(emulation): update Chrome userAgent to 66 (#4779)

--- lighthouse-core/lib/emulation.js
+++ lighthouse-core/lib/emulation.js
@@ -28,7 +28,7 @@ const NEXUS5X_EMULATION_METRICS = {
 
 const NEXUS5X_USERAGENT = {
   userAgent: 'Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5 Build/MRA58N) AppleWebKit/537.36' +
-    '(KHTML, like Gecko) Chrome/61.0.3116.0 Mobile Safari/537.36',
+    '(KHTML, like Gecko) Chrome/66.0.3359.30 Mobile Safari/537.36',
 };
 
 /**
