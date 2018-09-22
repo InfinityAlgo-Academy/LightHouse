417cdbef343dea66f23cb9328ff3da6926f9b446
Paul Irish <paul.irish@gmail.com>
2018-09-21 17:46:53 -0700

    new_audit: add js-libraries audit, just listing detected js libs (#6081)

--- typings/audit.d.ts
+++ typings/audit.d.ts
@@ -95,7 +95,7 @@ declare global {
 
     export type DetailsItem = string | number | DetailsRendererNodeDetailsJSON |
       DetailsRendererLinkDetailsJSON | DetailsRendererCodeDetailJSON | undefined |
-      boolean | DetailsRendererUrlDetailsJSON;
+      boolean | DetailsRendererUrlDetailsJSON | null;
 
     export interface DetailsRendererNodeDetailsJSON {
       type: 'node';
