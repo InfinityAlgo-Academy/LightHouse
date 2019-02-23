497c6a0f6c728e8768fdd05a0b5bf758a284eb61
Connor Clark <cjamcl@google.com>
2019-02-22 18:26:50 -0800

    header changes

--- lighthouse-core/report/html/renderer/report-renderer.js
+++ lighthouse-core/report/html/renderer/report-renderer.js
@@ -124,6 +124,36 @@ class ReportRenderer {
   }
 
   /**
+   * @param {LH.ReportResult[]} reports[]
+   * @return {DocumentFragment}
+   */
+  _renderDiffReportHeader(reports) {
+    const el = this._dom.cloneTemplate('#tmpl-lh-heading', this._templateContext);
+    const domFragment = this._dom.cloneTemplate('#tmpl-lh-scores-wrapper', this._templateContext);
+    const placeholder = this._dom.find('.lh-scores-wrapper-placeholder', el);
+    /** @type {HTMLDivElement} */ (placeholder.parentNode).replaceChild(domFragment, placeholder);
+    
+    const minFetchTime = Math.min(...reports.map(report => new Date(report.fetchTime).getTime()));
+    const maxFetchTime = Math.max(...reports.map(report => new Date(report.fetchTime).getTime()));
+    const minFetchTimeFormatted = Util.formatDateTime(new Date(minFetchTime).toDateString());
+    const maxFetchTimeFormatted = Util.formatDateTime(new Date(maxFetchTime).toDateString());
+    this._dom.find('.lh-config__timestamp', el).textContent =
+      `${minFetchTimeFormatted} - ${maxFetchTimeFormatted}`;
+    
+    // punt
+    this._dom.find('.lh-product-info__version', el).textContent = reports[0].lighthouseVersion;
+
+    this._dom.createChildOf(this._dom.find('.lh-metadata__results', el), 'div').textContent =
+      'Diff of Multiple Lighthouse Reports';
+    
+    this._dom.find('.lh-metadata__url', el).remove();
+    this._dom.find('.lh-toolbar__url', el).remove();
+    this._dom.find('.lh-config__emulation', el).remove();
+    
+    return el;
+  }
+
+  /**
    * @return {Element}
    */
   _renderReportShortHeader() {
@@ -210,9 +240,11 @@ class ReportRenderer {
     if (this._dom.isDevTools()) {
       headerContainer.classList.add('lh-header-plain');
       header = this._renderReportShortHeader();
-    } else {
-      // headerContainer.classList.add('lh-header-sticky');
+    } else if (reports.length === 1) {
+      headerContainer.classList.add('lh-header-sticky');
       header = this._renderReportHeader(baseReport);
+    } else {
+      header = this._renderDiffReportHeader(reports);
     }
     headerContainer.appendChild(header);
 
