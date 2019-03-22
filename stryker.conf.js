f4c7011d676923e6550f983d6a32ba1388ee2e7e
Brendan Kenny <bckenny@gmail.com>
2019-03-22 15:37:06 -0700

    more

--- stryker.conf.js
+++ stryker.conf.js
@@ -1,25 +1,31 @@
 'use strict';
 
+const JEST_WORKERS = 4;
+const STRYKER_WORKERS = 2;
+
 module.exports = function(config) {
   config.set({
     mutator: 'javascript',
     packageManager: 'yarn',
     testRunner: 'command',
-    commandRunner: {command: 'yarn tsc -p . && yarn unit-core --maxWorkers=4 --bail'},
+    commandRunner: {
+      command:
+          'yarn tsc -p . && ' +
+          `yarn unit-core --maxWorkers=${JEST_WORKERS} --bail`,
+    },
     transpilers: [],
     coverageAnalysis: 'off',
     mutate: [
-      'lighthouse-core/config/config.js',
-      'lighthouse-core/gather/gather-runner.js',
-      'lighthouse-core/runner.js',
+      'lighthouse-core/**/*.js',
+      '!lighthouse-core/scripts/**/*.js',
+      '!lighthouse-core/test/**/*.js',
     ],
     reporters: ['clear-text', 'progress', 'html'],
     htmlReporter: {
       baseDir: 'coverage/mutation/html',
     },
-    clearTextReporter: {logTests: true},
     // logLevel: 'all',
     timeoutMS: 80000,
-    maxConcurrentTestRunners: 2,
+    maxConcurrentTestRunners: STRYKER_WORKERS,
   });
 };
