797286513a1beb9bfb9ef56e48d28bdbfebdd106
Connor Clark <cjamcl@google.com>
2019-04-16 22:59:14 -0700

    pr changes

--- lighthouse-core/test/report/html/renderer/category-renderer-test.js
+++ lighthouse-core/test/report/html/renderer/category-renderer-test.js
@@ -145,11 +145,12 @@ describe('CategoryRenderer', () => {
     const audits = categoryDOM.querySelectorAll('.lh-audit');
     assert.equal(audits.length, category.auditRefs.length, 'renders correct number of audits');
 
-    assert.ok(categoryDOM.querySelector('.lh-gauge__wrapper--plugin') === null);
+    assert.equal(categoryDOM.querySelector('.lh-gauge__wrapper--plugin'), null);
   });
 
   it('plugin category has plugin badge', () => {
-    const category = sampleResults.reportCategories.find(c => c.id === 'seo');
+    const category = JSON.parse(
+      JSON.stringify(sampleResults.reportCategories.find(c => c.id === 'seo')));
     // Any category not of the core
     category.id = 'someplugin';
     const categoryDOM = renderer.render(category, sampleResults.categoryGroups);
