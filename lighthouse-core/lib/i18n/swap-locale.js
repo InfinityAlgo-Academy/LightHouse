81b0dc48c215c627ca72be1313fe523046b3c25b
Paul Irish <paulirish@google.com>
2019-04-22 13:42:59 -0700

    rename

--- lighthouse-core/lib/i18n/swap-locale.js
+++ lighthouse-core/lib/i18n/swap-locale.js
@@ -1,4 +1,3 @@
-#!/usr/bin/env node
 /**
  * @license Copyright 2019 Google Inc. All Rights Reserved.
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
@@ -8,35 +7,34 @@
 
 /* eslint-disable no-console, max-len */
 
-const fs = require('fs');
-const path = require('path');
 const _set = require('lodash.set');
 
-const i18n = require('../../lib/i18n/i18n.js');
+const i18n = require('./i18n.js');
 
 /**
  * Replaces all strings within an LHR with ones from a different locale
  * @param {LH.Result} lhr
- * @param {LH.Locale} locale
+ * @param {LH.Locale} requestedLocale
  */
-function replaceLHRLocale(lhr, locale) {
-  const resolvedLocale = i18n.lookupLocale(locale);
+function swapLocale(lhr, requestedLocale) {
+  const locale = i18n.lookupLocale(requestedLocale);
   const {icuMessagePaths} = lhr.i18n;
 
   Object.entries(icuMessagePaths).forEach(([icuMessageId, messageInstancesInLHR]) => {
     for (const instance of messageInstancesInLHR) {
+      // The path that _formatPathAsString() generated
       const path = /** @type {LH.I18NMessageValuesEntry} */ (instance).path || /** @type {string} */ (instance);
       const values = /** @type {LH.I18NMessageValuesEntry} */ (instance).values || undefined;
       // Get new formatted strings in revised locale
       const formattedStr = i18n.formatMessageFromIdWithValues(locale, icuMessageId, values);
-      // Write strings back into the LHR
+      // Write string back into the LHR
       _set(lhr, path, formattedStr);
     }
   });
 
   // Tweak the config locale
-  lhr.configSettings.locale = resolvedLocale;
+  lhr.configSettings.locale = locale;
   return lhr;
 }
 
-module.exports = replaceLHRLocale;
+module.exports = swapLocale;
