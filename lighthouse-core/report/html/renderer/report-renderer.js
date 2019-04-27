83bbdb1e19fac89279f50db91a61e5c1b3e6dd6a
Connor Clark <cjamcl@google.com>
2019-04-26 18:16:01 -0700

    report(redesign): fireworks

--- lighthouse-core/report/html/renderer/report-renderer.js
+++ lighthouse-core/report/html/renderer/report-renderer.js
@@ -244,16 +244,16 @@ class ReportRenderer {
 
     // TODO(hoten) - fireworks show will commence later.
     // Fireworks
-    // const scoresAll100 = report.reportCategories.every(cat => cat.score === 1);
-    // if (!this._dom.isDevTools() && scoresAll100) {
-    //   if (scoresAll100) {
-    //     const scoresContainer = this._dom.find('.lh-scores-container', headerContainer);
-    //     scoresContainer.classList.add('score100');
-    //     scoresContainer.addEventListener('click', _ => {
-    //       scoresContainer.classList.toggle('fireworks-paused');
-    //     });
-    //   }
-    // }
+    const scoresAll100 = report.reportCategories.every(cat => cat.score === 1);
+    if (!this._dom.isDevTools() && scoresAll100) {
+      if (scoresAll100) {
+        const scoresContainer = this._dom.find('.lh-scores-container', headerContainer);
+        scoresContainer.classList.add('score100');
+        scoresContainer.addEventListener('click', _ => {
+          scoresContainer.classList.toggle('fireworks-paused');
+        });
+      }
+    }
 
     if (scoreHeader) {
       const scoreGauges =
