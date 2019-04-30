8537a47b8fabf78a1e29400f2b511ec79d8e2ec5
Katie Hempenius <khempenius@users.noreply.github.com>
2019-04-30 13:57:30 -0400

    core(lightwallet): add resource-summary computed artifact (#8709)

--- types/budget.d.ts
+++ types/budget.d.ts
@@ -39,8 +39,8 @@ declare global {
       /** Supported timing metrics. */
       export type TimingMetric = 'first-contentful-paint' | 'first-cpu-idle' | 'interactive' | 'first-meaningful-paint' | 'estimated-input-latency';
 
-      /** Supported resource types. */
-      export type ResourceType = 'stylesheet' | 'image' | 'media' | 'font' | 'script' | 'document' | 'other';
+      /** Supported values for the resourceType property of a ResourceBudget. */
+      export type ResourceType = 'stylesheet' | 'image' | 'media' | 'font' | 'script' | 'document' | 'other' | 'total' | 'third-party';
     }
   }
 }
