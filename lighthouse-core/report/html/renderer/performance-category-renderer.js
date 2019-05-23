c20679963f0e622f09257bd9e1e4fba5bf5552e1
cjamcl <cjamcl@gmail.com>
2019-05-23 11:14:39 -0700

    renderer: fix word breaking of display text, move metric disclaimer (#9030)

--- lighthouse-core/report/html/renderer/performance-category-renderer.js
+++ lighthouse-core/report/html/renderer/performance-category-renderer.js
@@ -147,7 +147,7 @@ class PerformanceCategoryRenderer extends CategoryRenderer {
 
     // 'Values are estimated and may vary' is used as the category description for PSI
     if (environment !== 'PSI') {
-      const estValuesEl = this.dom.createChildOf(metricsColumn1El, 'div', 'lh-metrics__disclaimer');
+      const estValuesEl = this.dom.createChildOf(metricAuditsEl, 'div', 'lh-metrics__disclaimer');
       estValuesEl.textContent = Util.UIStrings.varianceDisclaimer;
     }
 
