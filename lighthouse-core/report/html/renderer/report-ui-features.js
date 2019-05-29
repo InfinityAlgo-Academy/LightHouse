b2016ea9ca46485d04af04413728fab7d2c0943d
Connor Clark <cjamcl@google.com>
2019-05-29 13:43:09 -0700

    comment

--- lighthouse-core/report/html/renderer/report-ui-features.js
+++ lighthouse-core/report/html/renderer/report-ui-features.js
@@ -577,6 +577,7 @@ class ReportUIFeatures {
     const el = this._dom.find('.lh-vars', this._document);
     // This seems unnecessary, but in DevTools, passing "undefined" as the second
     // parameter acts like passing "false".
+    // https://github.com/ChromeDevTools/devtools-frontend/blob/dd6a6d4153647c2a4203c327c595692c5e0a4256/front_end/dom_extension/DOMExtension.js#L809-L819
     if (typeof force === 'undefined') {
       el.classList.toggle('dark');
     } else {
