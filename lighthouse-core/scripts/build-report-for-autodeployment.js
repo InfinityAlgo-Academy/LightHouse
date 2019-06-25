f537af53c17e270dbd923c28584edbd901d2e0fe
Paul Irish <paulirish@google.com>
2019-06-25 10:02:59 -0700

    create an error lhr out of thin error

--- lighthouse-core/scripts/build-report-for-autodeployment.js
+++ lighthouse-core/scripts/build-report-for-autodeployment.js
@@ -14,34 +14,73 @@ const mkdirp = require('mkdirp').sync;
 const swapLocale = require('../lib/i18n/swap-locale.js');
 
 const ReportGenerator = require('../../lighthouse-core/report/report-generator.js');
+const GatherRunner = require('../gather/gather-runner.js');
+const {defaultSettings} = require('../config/constants.js');
+const lighthouse = require('../index.js');
 const lhr = /** @type {LH.Result} */ (require('../../lighthouse-core/test/results/sample_v2.json'));
 
-// Add a plugin to demo plugin rendering.
-lhr.categories['lighthouse-plugin-someplugin'] = {
-  id: 'lighthouse-plugin-someplugin',
-  title: 'Plugin',
-  score: 0.5,
-  auditRefs: [],
-};
+const DIST = path.join(__dirname, `../../dist/`);
 
 (async function() {
+  addPluginCategory(lhr);
+  const errorLhr = await generateErrorLHR();
+
   const filenameToLhr = {
-    'english': lhr,
-    'espanol': swapLocale(lhr, 'es').lhr,
-    'arabic': swapLocale(lhr, 'ar').lhr,
+    english: lhr,
+    espanol: swapLocale(lhr, 'es').lhr,
+    arabic: swapLocale(lhr, 'ar').lhr,
+    error: errorLhr,
   };
 
   // Generate and write reports
   Object.entries(filenameToLhr).forEach(([filename, lhr]) => {
     let html = ReportGenerator.generateReportHtml(lhr);
+    // TODO: PSI is another variant to consider
     for (const variant of ['', '-devtools']) {
       if (variant === '-devtools') {
-        html = html.replace(`"lh-root lh-vars"`, `"lh-root lh-vars lh-devtools"`)
+        // TODO: Make the DevTools Audits panel "emulation" more comprehensive
+        html = html.replace(`"lh-root lh-vars"`, `"lh-root lh-vars lh-devtools"`);
       }
-      const filepath = path.join(__dirname, `../../dist/${filename}${variant}/index.html`);
+      const filepath = `${DIST}${filename}${variant}/index.html`;
       mkdirp(path.dirname(filepath));
       fs.writeFileSync(filepath, html, {encoding: 'utf-8'});
       console.log('✅', filepath, 'written.');
     }
   });
 })();
+
+/**
+ * Add a plugin to demo plugin rendering.
+ * @param {LH.Result} lhr
+ */
+function addPluginCategory(lhr) {
+  lhr.categories['lighthouse-plugin-someplugin'] = {
+    id: 'lighthouse-plugin-someplugin',
+    title: 'Plugin',
+    score: 0.5,
+    auditRefs: [],
+  };
+}
+/**
+ * Generate an LHR with errors for the renderer to display
+ * We'll write an "empty" artifacts file to disk, only to use it in auditMode
+ * @return {Promise<LH.Result>}
+ */
+async function generateErrorLHR() {
+  const url = 'http://fakeurl.com';
+  const options = {
+    requestedUrl: url,
+    settings: defaultSettings,
+    driver: {
+      getBrowserVersion: () => ({userAgent: 'Mozilla/5.0 ErrorUserAgent Chrome/66'}),
+    },
+  };
+  //@ts-ignore driver isn't mocked out completely
+  const artifacts = await GatherRunner.initializeBaseArtifacts(options);
+
+  const TMP = `${DIST}/.tmp/`;
+  mkdirp(TMP);
+  fs.writeFileSync(`${TMP}/artifacts.json`, JSON.stringify(artifacts), 'utf-8');
+  const errorRunnerResult = await lighthouse(url, {auditMode: TMP});
+  return /** @type {LH.RunnerResult} */ (errorRunnerResult).lhr;
+}
