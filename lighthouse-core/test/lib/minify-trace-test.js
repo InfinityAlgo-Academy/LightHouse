03a0f1f710f0750119a8d5c65c9de6831bcbe9e6
Connor Clark <cjamcl@google.com>
2019-09-10 15:14:09 -0700

    update checked in artifacts

--- lighthouse-core/test/lib/minify-trace-test.js
+++ lighthouse-core/test/lib/minify-trace-test.js
@@ -14,7 +14,11 @@ const MetricsAudit = require('../../audits/metrics.js');
 
 describe('minify-trace', () => {
   it('has identical metrics to unminified', async () => {
-    const artifacts = {traces: {defaultPass: trace}, devtoolsLogs: {defaultPass: devtoolsLog}};
+    const artifacts = {
+      traces: {defaultPass: trace},
+      devtoolsLogs: {defaultPass: devtoolsLog},
+      Visibility: [],
+    };
     const context = {settings: {throttlingMethod: 'simulate'}, computedCache: new Map()};
     const {details: {items: [before]}} = await MetricsAudit.audit(artifacts, context);
     const beforeSize = JSON.stringify(trace).length;
