a2619196590a96017f639b6e17b40f0fedc61847
Connor Clark <cjamcl@google.com>
2019-09-11 12:50:58 -0700

    init

---
+++ lighthouse-core/scripts/lantern/collect-traces.js
@@ -0,0 +1,14 @@
+// 9 desktop, 9 mobile WPT
+
+const path = require('path');
+const fs = require('fs');
+
+const NUM_SAMPLES = 9;
+
+const outputFolder = path.join(__dirname, '..', '..', '..', 'dist', 'lantern-traces');
+
+async function main() {
+
+}
+
+main();
