419151adc09157ec4075f67ca4d5e4deb6082195
Connor Clark <cjamcl@google.com>
2019-09-11 23:44:56 -0700

    progress stuff

--- lighthouse-core/scripts/lantern/collect-traces.js
+++ lighthouse-core/scripts/lantern/collect-traces.js
@@ -1,9 +1,15 @@
-// 9 desktop, 9 mobile WPT
+/**
+ * @license Copyright 2019 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ */
+'use strict';
 
 const path = require('path');
 const fs = require('fs');
+const readline = require('readline');
 const https = require('https');
-const {execFileSync} = require('child_process');
+const {execFile} = require('child_process');
 
 const LH_ROOT = path.join(__dirname, '..', '..', '..');
 const SAMPLES = 2;
@@ -23,7 +29,7 @@ const summaryPath = path.join(outputFolder, 'summary.json');
 /** @typedef {{url: string, mobile: Result[], desktop: Result[]}} UrlResults */
 
 /** @type {UrlResults[]} */
-let summary = loadSummary();
+const summary = loadSummary();
 
 function loadSummary() {
   if (fs.existsSync(summaryPath)) {
@@ -65,7 +71,10 @@ async function startWptTest(url) {
   const apiUrl = new URL('https://www.webpagetest.org/runtest.php');
   apiUrl.searchParams.set('k', WPT_KEY);
   apiUrl.searchParams.set('f', 'json');
+  apiUrl.searchParams.set('location', 'Dulles:Chrome.3G');
   apiUrl.searchParams.set('lighthouse', '1');
+  apiUrl.searchParams.set('lighthouseTrace', '1');
+  apiUrl.searchParams.set('type', 'lighthouse');
   apiUrl.searchParams.set('mobile', '1');
   apiUrl.searchParams.set('url', url);
   const wptResponseJson = await fetch(apiUrl.href);
@@ -86,11 +95,16 @@ async function startWptTest(url) {
  */
 async function runForDesktop(url) {
   const artifactsFolder = `${LH_ROOT}/.tmp/collect-traces-artifacts`;
-  execFileSync('node', [
-    path.join(LH_ROOT, 'lighthouse-cli'),
-    url,
-    `-G=${artifactsFolder}`,
-  ] );
+  await new Promise((resolve, reject) => {
+    execFile('node', [
+      path.join(LH_ROOT, 'lighthouse-cli'),
+      url,
+      `-G=${artifactsFolder}`,
+    ], (_, stderr) => {
+      if (stderr) reject(stderr);
+      else resolve();
+    });
+  });
   const trace = fs.readFileSync(`${artifactsFolder}/defaultPass.trace.json`, 'utf-8');
   return {
     trace,
@@ -102,38 +116,35 @@ async function runForDesktop(url) {
  * @return {Promise<Result>}
  */
 async function runForMobile(url) {
-  return {trace:''};
-
   const {testId, jsonUrl} = await startWptTest(url);
-  if (DEBUG) console.log({testId, jsonUrl});
+  if (DEBUG) log({testId, jsonUrl});
 
-  // Give the test at least 30 seconds before we start polling.
-  await new Promise((resolve) => setTimeout(resolve, 30 * 1000));
-
-  // Poll for the results every 5 seconds.
-  const wptResult = await new Promise((resolve, reject) => {
-    const pollingInterval = 5 * 1000;
-
+  // Poll for the results every x seconds, where x = position in queue.
+  // This returns a response of {data: {lighthouse: {...}}}, but we don't
+  // care about the LHR so we ignore the response.
+  await new Promise((resolve, reject) => {
     async function poll() {
-      if (DEBUG) console.log('poll ...');
       const responseJson = await fetch(jsonUrl);
       const response = JSON.parse(responseJson);
-      if (response.statusCode === 200) return resolve(response);
-      if (response.statusCode >= 100 && response.statusCode < 200) return setTimeout(poll, pollingInterval);
-      reject(new Error(`unexpected response: ${response.statusCode} ${response.statusText}`));
+      if (response.statusCode === 200) {
+        resolve(response);
+      } else if (response.statusCode >= 100 && response.statusCode < 200) {
+        // If behindCount doesn't exist, the test is currently running.
+        const secondsToWait = response.data.behindCount || 5;
+        if (DEBUG) log('poll wpt in', secondsToWait);
+        setTimeout(poll, secondsToWait * 1000);
+      } else {
+        reject(new Error(`unexpected response: ${response.statusCode} ${response.statusText}`));
+      }
     }
 
     poll();
   });
-  const lhr = wptResult.data.lighthouse;
 
   const traceUrl = new URL('https://www.webpagetest.org/getgzip.php');
   traceUrl.searchParams.set('test', testId);
   traceUrl.searchParams.set('file', 'lighthouse_trace.json');
-  // TODO this isn't working ...
-  if (DEBUG) console.log(traceUrl.href);
-  // const traceJson = await fetch(traceUrl.href);
-  const traceJson = '';
+  const traceJson = await fetch(traceUrl.href);
 
   return {
     trace: traceJson,
@@ -148,12 +159,57 @@ async function repeatUntilPass(asyncFn) {
     try {
       return await asyncFn();
     } catch (err) {
-      console.log(err);
+      log(err);
     }
   }
 }
 
+let currentProgressMessage = '';
+
+/**
+ * @param  {...any} args
+ */
+function log(...args) {
+  readline.clearLine(process.stdout, 0);
+  readline.cursorTo(process.stdout, 0);
+  console.log(...args);
+  progress(currentProgressMessage);
+}
+
+/**
+ * @param {string} message
+ */
+function progress(message) {
+  currentProgressMessage = message;
+  readline.clearLine(process.stdout, 0);
+  readline.cursorTo(process.stdout, 0);
+  if (message) process.stdout.write(`${getNextLoadingChar()} ${message}`);
+}
+
+const loadingChars = '⣾⣽⣻⢿⡿⣟⣯⣷ ⠁⠂⠄⡀⢀⠠⠐⠈';
+let nextLoadingIndex = 0;
+function getNextLoadingChar() {
+  const char = loadingChars[nextLoadingIndex++];
+  if (nextLoadingIndex >= loadingChars.length) {
+    nextLoadingIndex = 0;
+  }
+  return char;
+}
+
+/** @type {NodeJS.Timer} */
+let progressBarHandle;
+function startProgressBar() {
+  progressBarHandle = setInterval(() => progress(currentProgressMessage), 100);
+}
+
+function stopProgressBar() {
+  if (progressBarHandle) clearInterval(progressBarHandle);
+  progress('');
+}
+
 async function main() {
+  startProgressBar();
+
   if (!fs.existsSync(outputFolder)) {
     fs.mkdirSync(outputFolder);
   }
@@ -162,6 +218,7 @@ async function main() {
     loadSummary();
   }
 
+  let urlIndex = 0;
   for (const urlResultSet of summary) {
     if (urlResultSet.mobile.length === SAMPLES && urlResultSet.desktop.length === SAMPLES) {
       continue;
@@ -169,35 +226,63 @@ async function main() {
 
     const url = urlResultSet.url;
     const sanitizedUrl = url.replace(/[^a-z0-9]/gi, '-');
-    console.log('collecting traces for', url);
+    log(`collecting traces for ${url}`);
 
-    const mobileResultPromises = [];
+    function updateProgress() {
+      const mobileDone = mobileResults.length === SAMPLES;
+      const desktopDone = desktopResults.length === SAMPLES;
+      progress([
+        `${url} (${urlIndex + 1} / ${URLS.length})`,
+        'mobile',
+        '(' + (mobileDone ? 'DONE' : `${mobileResults.length + 1} / ${SAMPLES}`) + ')',
+        'desktop',
+        '(' + (desktopDone ? 'DONE' : `${desktopResults.length + 1} / ${SAMPLES}`) + ')',
+      ].join(' '));
+    }
+
+    /** @type {Result[]} */
+    const mobileResults = [];
+    /** @type {Result[]} */
+    const desktopResults = [];
+
+    updateProgress();
+
+    const mobileResultsPromises = [];
     for (let i = 0; i < SAMPLES; i++) {
       // Can run in parallel.
-      mobileResultPromises.push(repeatUntilPass(() => runForMobile(url)));
+      const resultPromise = repeatUntilPass(() => runForMobile(url));
+      resultPromise.then((result) => mobileResults.push(result)).finally(updateProgress);
+      mobileResultsPromises.push(resultPromise);
     }
 
-    const desktopResults= [];
     for (let i = 0; i < SAMPLES; i++) {
       // Must run in series.
-      desktopResults.push(await repeatUntilPass(() => runForDesktop(url)));
+      const resultPromise = repeatUntilPass(() => runForDesktop(url));
+      desktopResults.push(await resultPromise);
+      updateProgress();
     }
 
-    const mobileTraces = await Promise.all(mobileResultPromises);
-    urlResultSet.mobile = mobileTraces.map((result, i) => {
+    await Promise.all(mobileResultsPromises);
+    urlResultSet.mobile = mobileResults.map((result, i) => {
       const traceFilename = `${sanitizedUrl}-mobile-${i + 1}-trace.json`;
       fs.writeFileSync(path.join(outputFolder, traceFilename), result.trace);
-      return {trace: traceFilename}
+      return {trace: traceFilename};
     });
 
     urlResultSet.desktop = desktopResults.map((result, i) => {
       const traceFilename = `${sanitizedUrl}-desktop-${i + 1}-trace.json`;
       fs.writeFileSync(path.join(outputFolder, traceFilename), result.trace);
-      return {trace: traceFilename}
+      return {trace: traceFilename};
     });
 
+    // We just collected NUM_SAMPLES * 2 traces, so let's save our progress.
     saveSummary();
+    progress('');
+    urlIndex++;
   }
+
+  log('done!');
+  stopProgressBar();
 }
 
 main();
