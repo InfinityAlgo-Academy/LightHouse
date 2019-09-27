0acf6334b88cc0cad3b72f5d07dc23bcdfa67c0e
Connor Clark <cjamcl@google.com>
2019-09-26 18:56:20 -0700

    golden

---
+++ lighthouse-core/scripts/lantern/collect/golden.js
@@ -0,0 +1,93 @@
+/**
+ * @license Copyright 2019 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ */
+'use strict';
+
+/** @typedef {{devtoolsLog?: string, lhr: string, trace: string}} Result */
+/** @typedef {{url: string, wpt: Result[], unthrottled: Result[]}} Summary */
+
+const fs = require('fs');
+const rimraf = require('rimraf');
+
+const LH_ROOT = `${__dirname}/../../../..`;
+
+const inputFolder = `${LH_ROOT}/dist/lantern-traces`;
+const summaryPath = `${inputFolder}/summary.json`;
+
+const outputFolder = `${LH_ROOT}/dist/golden-lantern-traces`;
+
+/**
+ * @return {Summary[]}
+ */
+function loadSummary() {
+  return JSON.parse(fs.readFileSync(summaryPath, 'utf-8'));
+}
+
+/**
+ * @param {string} filename
+ * @param {string} data
+ */
+function saveData(filename, data) {
+  fs.writeFileSync(`${outputFolder}/${filename}`, data);
+  return filename;
+}
+
+/**
+ * @param {string} filename
+ * @return {LH.Result}
+ */
+function loadLhr(filename) {
+  return JSON.parse(fs.readFileSync(`${inputFolder}/${filename}`, 'utf-8'));
+}
+
+/**
+ * Returns run w/ the median TTI.
+ * @param {Result[]} results
+ */
+function getMedianResult(results) {
+  const resultsWithValue = [];
+  for (const result of results) {
+    const lhr = loadLhr(result.lhr);
+    const metricsDetails = /** @type {LH.Audit.Details.DebugData=} */ (lhr.audits['metrics'].details);
+    /** @type {import('../../../audits/metrics.js').UberMetricsItem} */
+    const metrics = metricsDetails && metricsDetails.items && metricsDetails.items[0];
+    if (!metrics || !metrics.interactive) {
+      console.warn(`missing metrics: ${result.lhr}`);
+      continue;
+    }
+    resultsWithValue.push({value: metrics.interactive, result});
+  }  
+  resultsWithValue.sort((a, b) => b.value - a.value);
+
+  if (resultsWithValue.length % 2 === 1) return resultsWithValue[Math.floor(resultsWithValue.length / 2)].result;
+
+  // TODO
+  return resultsWithValue[Math.floor(resultsWithValue.length / 2)].result;
+}
+
+async function main() {
+  /** @type {Summary[]} */
+  const summary = loadSummary();
+
+  const golden = summary.map(({url, wpt, unthrottled}) => {
+    return {
+      url,
+      wpt: getMedianResult(wpt),
+      unthrottled: getMedianResult(unthrottled),
+    };
+  });
+
+  rimraf.sync(outputFolder);
+  fs.mkdirSync(outputFolder);
+  saveData('golden.json', JSON.stringify(golden, null, 2));
+  for (const result of golden) {
+    const filenames = [...Object.values(result.wpt), ...Object.values(result.unthrottled)].filter(Boolean);
+    for (const filename of filenames) {
+      fs.copyFileSync(`${inputFolder}/${filename}`, `${outputFolder}/${filename}`);
+    }
+  }
+}
+
+main();
