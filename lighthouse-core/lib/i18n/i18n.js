466bd6f79e57950d736de5edfa9642a40f1100fd
Jon Burger <jburger424@users.noreply.github.com>
2019-11-06 12:19:17 -0500

    i18n: Use log.verbose for outdated strings warning (#9931)

--- lighthouse-core/lib/i18n/i18n.js
+++ lighthouse-core/lib/i18n/i18n.js
@@ -291,7 +291,7 @@ function _formatIcuMessage(locale, icuMessageId, uiStringMessage, values = {}) {
 
     // Warn the user that the UIString message != the `en` message ∴ they should update the strings
     if (!LOCALES.en[icuMessageId] || localeMessage !== LOCALES.en[icuMessageId].message) {
-      log.warn('i18n', `Message "${icuMessageId}" does not match its 'en' counterpart. ` +
+      log.verbose('i18n', `Message "${icuMessageId}" does not match its 'en' counterpart. ` +
         `Run 'i18n' to update.`);
     }
   }
