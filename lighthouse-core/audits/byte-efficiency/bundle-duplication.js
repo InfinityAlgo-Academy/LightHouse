7f241efb53dd6ee93f6d34b84fa8018f57ccbcb6
Connor Clark <cjamcl@google.com>
2019-12-03 14:42:29 -0600

    fix size lookup

--- lighthouse-core/audits/byte-efficiency/bundle-duplication.js
+++ lighthouse-core/audits/byte-efficiency/bundle-duplication.js
@@ -259,8 +259,7 @@ class BundleDuplication extends ByteEfficiencyAudit {
 
         let sourceSize = 0;
         if (SIZE_MODE === 'moz' || SIZE_MODE === 'cdt') {
-          // moz: Takes ~2x as long naive approach
-          const fullSource = map.sourceRoot + source;
+          const fullSource = (map.sourceRoot || '') + source;
           sourceSize = fileSizes[fullSource];
         } else {
           if (!map.sourcesContent) continue;
