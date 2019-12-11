e6fde71183234d39f6fc301cc531b11200308863
Connor Clark <cjamcl@google.com>
2019-12-11 15:02:50 -0800

    semver

--- build/build-cdt-lib.js
+++ build/build-cdt-lib.js
@@ -35,7 +35,7 @@ for (const [input, output] of Object.entries(files)) {
     if (input.endsWith('SourceMap.js')) {
       if (line.includes('static load(')) deletionMode = true;
       if (line.includes('sourceContentProvider(')) deletionMode = true;
-      if (line.includes('Common.UIString')) newLine = '';
+      // if (line.includes('Common.UIString')) newLine = '';
       if (line.includes('export class WasmSourceMap')) deletionMode = true;
       if (line.includes('WasmSourceMap')) newLine = '';
       if (line.includes('export class EditResult')) deletionMode = true;
