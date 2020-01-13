1abf9b87963c1fb95654dceb7a6b5abe4477b2b9
Connor Clark <cjamcl@google.com>
2020-01-13 09:56:27 -0800

    core(resource-summary): ignore /favicon.ico (#10190)

--- lighthouse-core/computed/resource-summary.js
+++ lighthouse-core/computed/resource-summary.js
@@ -51,6 +51,12 @@ class ResourceSummary {
 
     for (const record of networkRecords) {
       const type = this.determineResourceType(record);
+      if (type === 'other' && record.url.endsWith('/favicon.ico')) {
+        // Headless Chrome does not request /favicon.ico, so don't consider this request.
+        // Makes resource summary consistent across LR / other channels.
+        continue;
+      }
+
       resourceSummary[type].count++;
       resourceSummary[type].size += record.transferSize;
 
