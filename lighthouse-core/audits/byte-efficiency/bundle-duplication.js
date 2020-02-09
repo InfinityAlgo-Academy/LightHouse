61a37dec2de42a8e1aefb81da921e6729a50414d
Connor Clark <cjamcl@google.com>
2020-02-08 22:13:57 -0800

    update

--- lighthouse-core/audits/byte-efficiency/bundle-duplication.js
+++ lighthouse-core/audits/byte-efficiency/bundle-duplication.js
@@ -1,5 +1,5 @@
 /**
- * @license Copyright 2017 Google Inc. All Rights Reserved.
+ * @license Copyright 2020 Google Inc. All Rights Reserved.
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
  */
@@ -107,13 +107,13 @@ class BundleDuplication extends ByteEfficiencyAudit {
     }
 
     /**
-     * @typedef ItemMulti
+     * @typedef ItemSubrows
      * @property {string[]} urls
      * @property {number[]} sourceBytes
      */
 
     /**
-     * @typedef {LH.Audit.ByteEfficiencyItem & ItemMulti} Item
+     * @typedef {LH.Audit.ByteEfficiencyItem & ItemSubrows} Item
      */
 
     /** @type {Item[]} */
