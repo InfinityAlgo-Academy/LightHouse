f3bc037a55d8434377104547673f9e1beb79cc21
Connor Clark <cjamcl@google.com>
2020-04-27 19:08:50 -0700

    zone

--- lighthouse-core/scripts/lantern/collect/gcp-create-and-run.sh
+++ lighthouse-core/scripts/lantern/collect/gcp-create-and-run.sh
@@ -12,10 +12,10 @@ gcloud --project="$CLOUDSDK_CORE_PROJECT" compute instances create url-collect-i
   --boot-disk-size=100GB \
   --machine-type=n1-standard-2
 
-gcloud --project="$CLOUDSDK_CORE_PROJECT" compute scp ./lighthouse-core/scripts/lantern/collect/gcp-setup.sh url-collect-instance:/tmp/gcp-setup.sh
-gcloud --project="$CLOUDSDK_CORE_PROJECT" compute scp ./lighthouse-core/scripts/lantern/collect/gcp-run.sh url-collect-instance:/tmp/gcp-run.sh
-gcloud --project="$CLOUDSDK_CORE_PROJECT" compute ssh url-collect-instance --command="bash /tmp/gcp-setup.sh"
-gcloud --project="$CLOUDSDK_CORE_PROJECT" compute ssh url-collect-instance --command="sudo -u lighthouse sh -c 'nohup /home/lighthouse/gcp-run.sh > /home/lighthouse/collect.log 2>&1 < /dev/null &'"
+gcloud --project="$CLOUDSDK_CORE_PROJECT" compute scp ./lighthouse-core/scripts/lantern/collect/gcp-setup.sh url-collect-instance:/tmp/gcp-setup.sh --zone=us-central1-a
+gcloud --project="$CLOUDSDK_CORE_PROJECT" compute scp ./lighthouse-core/scripts/lantern/collect/gcp-run.sh url-collect-instance:/tmp/gcp-run.sh --zone=us-central1-a
+gcloud --project="$CLOUDSDK_CORE_PROJECT" compute ssh url-collect-instance --command="bash /tmp/gcp-setup.sh" --zone=us-central1-a
+gcloud --project="$CLOUDSDK_CORE_PROJECT" compute ssh url-collect-instance --command="sudo -u lighthouse sh -c 'nohup /home/lighthouse/gcp-run.sh > /home/lighthouse/collect.log 2>&1 < /dev/null &'" --zone=us-central1-a
 
 set +x
 
