3a5cd1efe8510e60997474775698bdc22ac3ed9d
Connor Clark <cjamcl@google.com>
2020-06-15 18:20:40 -0700

    enable

--- lighthouse-cli/test/smokehouse/test-definitions/perf/expectations.js
+++ lighthouse-cli/test/smokehouse/test-definitions/perf/expectations.js
@@ -175,16 +175,15 @@ module.exports = [
             ],
           },
         },
-        // TODO: uncomment when Chrome m84 lands
-        // 'layout-shift-elements': {
-        //   score: null,
-        //   displayValue: '2 elements found',
-        //   details: {
-        //     items: {
-        //       length: 2,
-        //     },
-        //   },
-        // },
+        'layout-shift-elements': {
+          score: null,
+          displayValue: '2 elements found',
+          details: {
+            items: {
+              length: 2,
+            },
+          },
+        },
         'long-tasks': {
           score: null,
           details: {
