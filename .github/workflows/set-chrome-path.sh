c2a88d4be02bd251a833a1cb0f04b7cfd6b4c4d4
Connor Clark <cjamcl@google.com>
2020-06-18 17:21:07 -0700

    wip

--- .github/workflows/set-chrome-path.sh
+++ .github/workflows/set-chrome-path.sh
@@ -6,7 +6,7 @@
 # Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 ##
 
-if [[ "$1" == "stable" ]]
+if [[ "$1" == "stable" ]] ; then
   echo '::set-env name=CHROME_PATH::'
 elif [[ "$1" == "ToT" ]]
   echo '::set-env name=CHROME_PATH::/home/runner/chrome-linux-tot/chrome'
