4ab64afd1c2608298397f665e67374782b6f10d8
Connor Clark <cjamcl@google.com>
2020-07-28 18:08:18 -0500

    ok

--- lighthouse-core/scripts/chromium-web-tests/test.sh
+++ lighthouse-core/scripts/chromium-web-tests/test.sh
@@ -6,19 +6,21 @@
 # Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 ##
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 # Setup dependencies.
 export DEPOT_TOOLS=~/tools/depot-tools
 export DEVTOOLS_PATH=~/tmp/devtools/devtools-frontend
 export BLINK_TOOLS_PATH=~/tmp/blink_tools
 export PATH=$DEPOT_TOOLS_PATH:$PATH
 
-bash lighthouse-core/scripts/chromium-web-tests/download-depot-tools.sh
-bash lighthouse-core/scripts/chromium-web-tests/download-devtools.sh
-bash lighthouse-core/scripts/chromium-web-tests/download-blink-tools.sh
-node lighthouse-core/scripts/chromium-web-tests/download-content-shell.js
+bash "$SCRIPT_DIR/download-depot-tools.sh"
+bash "$SCRIPT_DIR/download-devtools.sh"
+bash "$SCRIPT_DIR/download-blink-tools.sh"
+node "$SCRIPT_DIR/download-content-shell.js"
 
 # Run web tests.
-bash lighthouse-core/scripts/chromium-web-tests/run-web-tests.sh
+bash "$SCRIPT_DIR/run-web-tests.sh"
 
 # Reset the results.
-bash lighthouse-core/scripts/chromium-web-tests/run-web-tests.sh --reset-results
+bash "$SCRIPT_DIR/run-web-tests.sh" --reset-results
