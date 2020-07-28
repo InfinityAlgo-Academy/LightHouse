e3f52406a1d5e0206d7b0592385456870ff5b0eb
Connor Clark <cjamcl@google.com>
2020-07-28 12:30:31 -0500

    keep webtests in lh repo

--- lighthouse-core/scripts/chromium-web-tests/README.md
+++ lighthouse-core/scripts/chromium-web-tests/README.md
@@ -3,9 +3,13 @@
 ## Running
 
 ```sh
+# Setup dependencies.
+git clone https://chromium.googlesource.com/devtools/devtools-frontend.git ~/tmp/devtools
 BLINK_TOOLS_PATH=~/tmp/blink_tools bash lighthouse-core/scripts/chromium-web-tests/download-blink-tools.sh
 node lighthouse-core/scripts/chromium-web-tests/download-content-shell.js
-BLINK_TOOLS_PATH=~/tmp/blink_tools DEVTOOLS_PATH=~/src/devtools/devtools-frontend bash lighthouse-core/scripts/chromium-web-tests/run-web-tests.sh
+
+# Run web tests.
+BLINK_TOOLS_PATH=~/tmp/blink_tools DEVTOOLS_PATH=~/tmp/devtools bash lighthouse-core/scripts/chromium-web-tests/run-web-tests.sh
 ```
 
 ## Modifying blink-tools
--- lighthouse-core/scripts/chromium-web-tests/run-web-tests.sh
+++ lighthouse-core/scripts/chromium-web-tests/run-web-tests.sh
@@ -18,7 +18,7 @@ for file in "$LH_ROOT/.test-cache"/*/; do
   [[ $file -nt $latest_content_shell ]] && latest_content_shell=$file
 done
 
-if [ x"$latest_content_shell" == x ]; then
+if [[ $* == *--flag* ]]; then
   echo "Error: Did not find a content shell"
   exit 1
 fi
@@ -32,6 +32,9 @@ rm -rf "$latest_content_shell/out/Release/layout-test-results"
 
 # Setup inspector-sources
 ln -s "$latest_content_shell/out/Release/resources/inspector" "$DEVTOOLS_PATH/test/webtests/http/tests/inspector-sources"
+# Setup lighthouse webtests
+rm -rf "$DEVTOOLS_PATH/test/webtests/http/tests/devtools/lighthouse"
+ln -s "$SCRIPT_DIR/webtests/http/tests/devtools/lighthouse" "$DEVTOOLS_PATH/test/webtests/http/tests/devtools/lighthouse"
 
 # Kill background jobs when script ends.
 cleanup() {
@@ -54,13 +57,16 @@ echo "Server is up"
 
 # Add typ to python path. The regular method assumes there is a chromium checkout.
 # See https://source.chromium.org/chromium/chromium/src/+/master:third_party/blink/tools/blinkpy/common/path_finder.py;l=35;drc=61e88d0e7fa9217a8f5395edd0e03b1c1991257c
+set -o xtrace
 PYTHONPATH="${PYTHONPATH}:$BLINK_TOOLS_PATH/third_party/typ" python \
   "$BLINK_TOOLS_PATH/third_party/blink/tools/run_web_tests.py" \
   --layout-tests-directory="$DEVTOOLS_PATH/test/webtests" \
   --build-directory="$latest_content_shell/out" \
+  $* \
   http/tests/devtools/lighthouse
 status=$?
+set +o xtrace
 
-cp -r "$latest_content_shell/out/Release/layout-test-results" "$LH_ROOT/.test-cache/layout-test-results"
+cp -r "$latest_content_shell/out/Release/layout-test-results" "$LH_ROOT/.tmp/layout-test-results"
 
 exit $status
