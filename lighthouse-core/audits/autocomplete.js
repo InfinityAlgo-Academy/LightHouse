f6be6dbca9f64f69b69f1de9fe2a07762a49ef73
George Martin <martingeorge@google.com>
2020-08-13 18:12:09 -0400

    gatherer automatically does not collect invalid attributes, simplified audit

--- lighthouse-core/audits/autocomplete.js
+++ lighthouse-core/audits/autocomplete.js
@@ -29,17 +29,6 @@ const UIStrings = {
 
 const str_ = i18n.createMessageInstanceIdFn(__filename, UIStrings);
 
-/** @type {string[]} This array contains all acceptable autocomplete attributes from the WHATWG standard. More found at https://html.spec.whatwg.org/multipage/form-control-infrastructure.html#autofill */
-const validAutocompleteAttributeNames = ['name', 'honorific-prefix', 'given-name',
-  'additional-name', 'family-name', 'honorific-suffix', 'nickname', 'username', 'new-password',
-  'current-password', 'one-time-code', 'organization-title', 'organization', 'street-address',
-  'address-line1', 'address-line2', 'address-line3', 'address-level4', 'address-level3',
-  'address-level2', 'address-level1', 'country', 'country-name', 'postal-code', 'cc-name',
-  'cc-given-name', 'cc-additional-name', 'cc-family-name', 'cc-number', 'cc-exp',
-  'cc-exp-month', 'cc-exp-year', 'cc-csc', 'cc-type', 'transaction-currency',
-  'transaction-amount', 'language', 'bday', 'bday-day', 'bday-month', 'bday-year',
-  'sex', 'url', 'photo', 'tel', 'tel-country-code', 'tel-national', 'tel-area-code', 'on',
-  'tel-local', 'tel-local-prefix', 'tel-local-suffix', 'tel-extension', 'email', 'impp', 'off'];
 
 class AutocompleteAudit extends Audit {
   /**
@@ -56,19 +45,6 @@ class AutocompleteAudit extends Audit {
   }
 
   /**
-   * @param {LH.Artifacts.FormInput} input
-   * @return {Boolean}
-   */
-  static isValidAutocomplete(input) {
-    for (const name of validAutocompleteAttributeNames) {
-      if (input.autocomplete.includes(name)) {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  /**
    * @param {LH.Artifacts} artifacts
    * @return {LH.Audit.Product}
    */
@@ -77,7 +53,7 @@ class AutocompleteAudit extends Audit {
     const failingFormsData = [];
     for (const form of forms) {
       for (const input of form.inputs) {
-        if (!this.isValidAutocomplete(input)) {
+        if (!input.autocomplete) {
           failingFormsData.push({
             failingElements: /** @type {LH.Audit.Details.NodeValue} */ ({
               type: 'node',
