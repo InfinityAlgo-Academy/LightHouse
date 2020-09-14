84be010d33b2645011494498c6b26b8010641528
Mohammed J. Razem <m.razem@vardot.com>
2020-09-14 19:57:57 +0300

    core(stack-packs): add Drupal pack (#10522)

--- lighthouse-core/lib/stack-packs.js
+++ lighthouse-core/lib/stack-packs.js
@@ -32,6 +32,10 @@ const stackPacksToInclude = [
     requiredStacks: ['js:wordpress'],
   },
   {
+    packId: 'drupal',
+    requiredStacks: ['js:drupal'],
+  },
+  {
     packId: 'react',
     requiredStacks: ['js:react'],
   },
