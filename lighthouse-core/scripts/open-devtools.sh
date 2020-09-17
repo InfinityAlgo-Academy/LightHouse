a4f81cfd79086c02da3a13c9bc1ca87e74a1fe80
Connor Clark <cjamcl@google.com>
2020-09-17 15:42:34 -0500

    feedback

--- lighthouse-core/scripts/open-devtools.sh
+++ lighthouse-core/scripts/open-devtools.sh
@@ -8,13 +8,24 @@ set -euo pipefail
 # Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 ##
 
-if [[ -z $CHROME_PATH ]]
-then
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+LH_ROOT="$SCRIPT_DIR/../.."
+TEST_DIR="$LH_ROOT/.tmp/chromium-web-tests"
+DEFAULT_DEVTOOLS_PATH="$TEST_DIR/devtools/devtools-frontend"
+DEVTOOLS_PATH=${DEVTOOLS_PATH:-"$DEFAULT_DEVTOOLS_PATH"}
+
+if [[ -z $CHROME_PATH ]]; then
   echo 'Must set $CHROME_PATH'
   exit 1
 fi
 
-export DEVTOOLS_PATH=${DEVTOOLS_PATH:-"$HOME/src/devtools/devtools-frontend"}
+echo "CHROME_PATH: $CHROME_PATH"
+echo "DEVTOOLS_PATH: $DEVTOOLS_PATH"
+
+if [ ! -d $DEVTOOLS_PATH ]; then
+  echo "No devtools found at $DEVTOOLS_PATH. Have you run 'yarn test-devtools' yet?"
+  exit 1
+fi
 
 yarn build-devtools
 yarn devtools "$DEVTOOLS_PATH"
