385ab501e8cf09fe292f96da0cea27fa3671c8c6
Quentin <wrightq00@gmail.com>
2020-09-21 16:33:18 -0500

    core(is-on-https): add missing space in description (#11466)

--- lighthouse-core/audits/is-on-https.js
+++ lighthouse-core/audits/is-on-https.js
@@ -18,7 +18,7 @@ const UIStrings = {
   /** Description of a Lighthouse audit that tells the user *why* HTTPS use *for all resources* is important. This is displayed after a user expands the section to see more. No character length limits. 'Learn More' becomes link text to additional documentation. */
   description: 'All sites should be protected with HTTPS, even ones that don\'t handle ' +
       'sensitive data. This includes avoiding [mixed content](https://developers.google.com/web/fundamentals/security/prevent-mixed-content/what-is-mixed-content), ' +
-      'where some resources are loaded over HTTP despite the initial request being served' +
+      'where some resources are loaded over HTTP despite the initial request being served ' +
       'over HTTPS. HTTPS prevents intruders from tampering with or passively listening ' +
       'in on the communications between your app and your users, and is a prerequisite for ' +
       'HTTP/2 and many new web platform APIs. ' +
