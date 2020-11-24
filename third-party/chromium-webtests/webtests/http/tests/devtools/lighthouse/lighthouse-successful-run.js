8c14a60a4b6d8b99ee558bb84d94418551c40127
Connor Clark <cjamcl@google.com>
2020-11-23 18:24:29 -0600

    tests: rebaseline devtools test, print each .lh-audit id (#11702)

--- third-party/chromium-webtests/webtests/http/tests/devtools/lighthouse/lighthouse-successful-run.js
+++ third-party/chromium-webtests/webtests/http/tests/devtools/lighthouse/lighthouse-successful-run.js
@@ -79,8 +79,9 @@
   });
 
   const resultsElement = LighthouseTestRunner.getResultsElement();
-  const auditElements = resultsElement.querySelectorAll('.lh-audit');
+  const auditElements = [...resultsElement.querySelectorAll('.lh-audit')];
   TestRunner.addResult(`\n# of .lh-audit divs: ${auditElements.length}`);
+  TestRunner.addResult(`\n.lh-audit divs:\n${auditElements.map(e => e.id).join('\n')}`);
 
   TestRunner.completeTest();
 })();
