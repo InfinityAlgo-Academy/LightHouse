c78f3287c0e8da5a286c84afb734e254000f4672
Paul Irish <paulirish@google.com>
2020-11-30 19:53:01 -0800

    score diags w/ audit score. (and hoepfully not any merge of opps and diags)

--- lighthouse-core/report/html/renderer/performance-category-renderer.js
+++ lighthouse-core/report/html/renderer/performance-category-renderer.js
@@ -249,7 +249,7 @@ class PerformanceCategoryRenderer extends CategoryRenderer {
     if (opportunityAudits.length) {
       // Scale the sparklines relative to savings, minimum 2s to not overstate small savings
       const minimumScale = 2000;
-      const wastedMsValues = opportunityAudits.map(audit => this._getWastedMs(audit));
+      const wastedMsValues = opportunityAudits.map(audit => audit.group === 'load-opportunities' && this._getWastedMs(audit));
       const maxWaste = Math.max(...wastedMsValues);
       const scale = Math.max(Math.ceil(maxWaste / 1000) * 1000, minimumScale);
       const groupEl = this.renderAuditGroup(groups['load-opportunities']);
