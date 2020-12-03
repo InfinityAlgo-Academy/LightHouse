ba75d8d4a5f8b1fd3ccb4f1824a083bb019e7e67
Warren Maresca <warrengm@google.com>
2020-12-03 18:59:09 -0500

    misc(build): fix bundling publisher ads plugin in Lightrider (#11648)

--- lighthouse-core/config/config.js
+++ lighthouse-core/config/config.js
@@ -435,7 +435,7 @@ class Config {
       assertValidPluginName(configJSON, pluginName);
 
       // TODO: refactor and delete `global.isDevtools`.
-      const pluginPath = global.isDevtools ?
+      const pluginPath = global.isDevtools || global.isLightrider ?
         pluginName :
         resolveModule(pluginName, configDir, 'plugin');
       const rawPluginJson = require(pluginPath);
