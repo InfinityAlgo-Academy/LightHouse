3f75a30f44bc998c26fd1fd5eb2a09bf3d8a0272
Paul Irish <paulirish@google.com>
2020-12-08 15:43:25 -0800

    ff only

--- lighthouse-core/test/chromium-web-tests/download-devtools.sh
+++ lighthouse-core/test/chromium-web-tests/download-devtools.sh
@@ -16,7 +16,7 @@ then
   # Update to keep current
   git reset --hard
   git clean -fd
-  git pull -f origin master
+  git pull --ff-only --force origin master
   gclient sync --delete_unversioned_trees --reset
 
   exit 0
