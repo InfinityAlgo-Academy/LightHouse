791d15d29c9343605b7fcdebddbeced6fe16c257
Paul Irish <paulirish@google.com>
2020-12-10 12:36:19 -0800

    tests: fix unbound variable in download-devtools.sh (#11809)

--- lighthouse-core/test/chromium-web-tests/download-devtools.sh
+++ lighthouse-core/test/chromium-web-tests/download-devtools.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+set -euxo pipefail
 
 ##
 # @license Copyright 2020 The Lighthouse Authors. All Rights Reserved.
@@ -18,7 +18,7 @@ then
   
   # Update to keep current.
   # Don't update in CI-defer to the weekly cache invalidation.
-  if ! [[ "$CI" ]]; then
+  if [ -n "${CI:-}" ]; then
     git reset --hard
     git clean -fd
     git pull --ff-only -f origin master
