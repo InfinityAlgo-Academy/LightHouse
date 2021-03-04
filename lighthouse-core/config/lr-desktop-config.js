25b6b397144dacd99024ea69199fb8646896451b
Connor Clark <cjamcl@google.com>
2021-03-03 17:16:00 -0700

    clients(lr): enable uses-http2

--- lighthouse-core/config/lr-desktop-config.js
+++ lighthouse-core/config/lr-desktop-config.js
@@ -17,8 +17,6 @@ const config = {
     throttling: constants.throttling.desktopDense4G,
     screenEmulation: constants.screenEmulationMetrics.desktop,
     emulatedUserAgent: constants.userAgents.desktop,
-    // Skip the h2 audit so it doesn't lie to us. See https://github.com/GoogleChrome/lighthouse/issues/6539
-    skipAudits: ['uses-http2'],
   },
 };
 
