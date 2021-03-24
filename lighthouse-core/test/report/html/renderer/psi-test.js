c9d846c2ec930866ad63602a4469eeb7dda02fb1
Connor Clark <cjamcl@google.com>
2021-03-23 19:27:17 -0500

    clients(psi): support element screenshots (#12288)

--- lighthouse-core/test/report/html/renderer/psi-test.js
+++ lighthouse-core/test/report/html/renderer/psi-test.js
@@ -19,6 +19,8 @@ const CategoryRenderer = require('../../../../report/html/renderer/category-rend
 const DetailsRenderer = require('../../../../report/html/renderer/details-renderer.js');
 const CriticalRequestChainRenderer =
     require('../../../../report/html/renderer/crc-details-renderer.js');
+const ElementScreenshotRenderer =
+    require('../../../../report/html/renderer/element-screenshot-renderer.js');
 
 const {itIfProtoExists, sampleResultsRoundtripStr} = testUtils.getProtoRoundTrip();
 const sampleResultsStr = fs.readFileSync(__dirname + '/../../../results/sample_v2.json', 'utf-8');
@@ -45,6 +47,7 @@ describe('DOM', () => {
         require('../../../../report/html/renderer/performance-category-renderer.js');
     global.PerformanceCategoryRenderer = PerformanceCategoryRenderer;
     global.CriticalRequestChainRenderer = CriticalRequestChainRenderer;
+    global.ElementScreenshotRenderer = ElementScreenshotRenderer;
 
     const {window} = new jsdom.JSDOM(TEMPLATE_FILE);
     document = window.document;
@@ -58,6 +61,7 @@ describe('DOM', () => {
     global.DetailsRenderer = undefined;
     global.PerformanceCategoryRenderer = undefined;
     global.CriticalRequestChainRenderer = undefined;
+    global.ElementScreenshotRenderer = undefined;
   });
 
   describe('psi prepareLabData helpers', () => {
