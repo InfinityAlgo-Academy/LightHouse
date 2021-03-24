20c8080d28507affed505c55c1b47179a8ccc648
Adam Raine <6752989+adamraine@users.noreply.github.com>
2021-03-24 19:05:18 -0400

    core(csp-xss): hidden severity (#12240)

--- lighthouse-core/audits/csp-xss.js
+++ lighthouse-core/audits/csp-xss.js
@@ -74,17 +74,19 @@ class CspXss extends Audit {
 
   /**
    * @param {Finding} finding
+   * @param {string=} icon
    * @return {LH.Audit.Details.TableItem}
    */
-  static findingToTableItem(finding) {
+  static findingToTableItem(finding, icon) {
     return {
       directive: finding.directive,
       description: getTranslatedDescription(finding),
+      severity: icon,
     };
   }
 
   /**
-   * @param {import('../lib/csp-evaluator').Finding[][]} syntaxFindings
+   * @param {Finding[][]} syntaxFindings
    * @param {string[]} rawCsps
    * @return {LH.Audit.Details.TableItem[]}
    */
@@ -93,9 +95,10 @@ class CspXss extends Audit {
     const results = [];
 
     for (let i = 0; i < syntaxFindings.length; ++i) {
-      const items = syntaxFindings[i].map(this.findingToTableItem);
+      const items = syntaxFindings[i].map(f => this.findingToTableItem(f));
       if (!items.length) continue;
       results.push({
+        severity: 'Syntax',
         description: {
           type: 'code',
           value: rawCsps[i],
@@ -111,34 +114,51 @@ class CspXss extends Audit {
   }
 
   /**
+   * @param {string[]} cspHeaders
+   * @param {string[]} cspMetaTags
+   * @return {{score: number, results: LH.Audit.Details.TableItem[]}}
+   */
+  static constructResults(cspHeaders, cspMetaTags) {
+    const rawCsps = [...cspHeaders, ...cspMetaTags];
+    if (!rawCsps.length) {
+      return {
+        score: 0,
+        results: [{
+          severity: 'Bypass',
+          description: str_(UIStrings.noCsp),
+          directive: undefined,
+        }],
+      };
+    }
+
+    const {bypasses, warnings, syntax} = evaluateRawCspsForXss(rawCsps);
+
+    const results = [
+      ...this.constructSyntaxResults(syntax, rawCsps),
+      ...bypasses.map(f => this.findingToTableItem(f, 'Bypass')),
+      ...warnings.map(f => this.findingToTableItem(f, 'Warning')),
+    ];
+
+    // Add extra warning for a CSP defined in a meta tag.
+    if (cspMetaTags.length) {
+      results.push({
+        severity: 'Warning',
+        description: str_(UIStrings.metaTagMessage),
+        directive: undefined,
+      });
+    }
+
+    return {score: bypasses.length ? 0 : 1, results};
+  }
+
+  /**
    * @param {LH.Artifacts} artifacts
    * @param {LH.Audit.Context} context
    * @return {Promise<LH.Audit.Product>}
    */
   static async audit(artifacts, context) {
     const {cspHeaders, cspMetaTags} = await this.getRawCsps(artifacts, context);
-    const rawCsps = [...cspHeaders, ...cspMetaTags];
-    if (!rawCsps.length) {
-      return {
-        score: 0,
-        notApplicable: false,
-        displayValue: str_(UIStrings.noCsp),
-      };
-    }
-
-    // TODO: Add severity icons for bypasses and warnings.
-    const {bypasses, warnings, syntax} = evaluateRawCspsForXss(rawCsps);
-
-    const results = [
-      ...this.constructSyntaxResults(syntax, rawCsps),
-      ...bypasses.map(this.findingToTableItem),
-      ...warnings.map(this.findingToTableItem),
-    ];
-
-    // Add extra warning for a CSP defined in a meta tag.
-    if (cspMetaTags.length) {
-      results.push({description: str_(UIStrings.metaTagMessage), directive: undefined});
-    }
+    const {score, results} = this.constructResults(cspHeaders, cspMetaTags);
 
     /** @type {LH.Audit.Details.Table['headings']} */
     const headings = [
@@ -148,8 +168,9 @@ class CspXss extends Audit {
       /* eslint-enable max-len */
     ];
     const details = Audit.makeTableDetails(headings, results);
+
     return {
-      score: bypasses.length ? 0 : 1,
+      score,
       notApplicable: false,
       details,
     };
