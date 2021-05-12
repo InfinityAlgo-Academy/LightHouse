40b8df888cb13dc6123a3eec84e6a94efe4dfe05
Patrick Hulce <patrick.hulce@gmail.com>
2021-05-11 22:12:28 -0500

    core(fr): extract navigation errors from gather-runner (#12461)

--- lighthouse-core/gather/gather-runner.js
+++ lighthouse-core/gather/gather-runner.js
@@ -6,8 +6,7 @@
 'use strict';
 
 const log = require('lighthouse-logger');
-const LHError = require('../lib/lh-error.js');
-const NetworkAnalyzer = require('../lib/dependency-graph/simulator/network-analyzer.js');
+const {getPageLoadError} = require('../lib/navigation-error.js');
 const NetworkRecorder = require('../lib/network-recorder.js');
 const emulation = require('../lib/emulation.js');
 const constants = require('../config/constants.js');
@@ -221,136 +220,6 @@ class GatherRunner {
       }
     }
     log.timeEnd(status);
-  }
-
-  /**
-   * Returns an error if the original network request failed or wasn't found.
-   * @param {LH.Artifacts.NetworkRequest|undefined} mainRecord
-   * @return {LH.LighthouseError|undefined}
-   */
-  static getNetworkError(mainRecord) {
-    if (!mainRecord) {
-      return new LHError(LHError.errors.NO_DOCUMENT_REQUEST);
-    } else if (mainRecord.failed) {
-      const netErr = mainRecord.localizedFailDescription;
-      // Match all resolution and DNS failures
-      // https://cs.chromium.org/chromium/src/net/base/net_error_list.h?rcl=cd62979b
-      if (
-        netErr === 'net::ERR_NAME_NOT_RESOLVED' ||
-        netErr === 'net::ERR_NAME_RESOLUTION_FAILED' ||
-        netErr.startsWith('net::ERR_DNS_')
-      ) {
-        return new LHError(LHError.errors.DNS_FAILURE);
-      } else {
-        return new LHError(
-          LHError.errors.FAILED_DOCUMENT_REQUEST,
-          {errorDetails: netErr}
-        );
-      }
-    } else if (mainRecord.hasErrorStatusCode()) {
-      return new LHError(
-        LHError.errors.ERRORED_DOCUMENT_REQUEST,
-        {statusCode: `${mainRecord.statusCode}`}
-      );
-    }
-  }
-
-  /**
-   * Returns an error if we ended up on the `chrome-error` page and all other requests failed.
-   * @param {LH.Artifacts.NetworkRequest|undefined} mainRecord
-   * @param {Array<LH.Artifacts.NetworkRequest>} networkRecords
-   * @return {LH.LighthouseError|undefined}
-   */
-  static getInterstitialError(mainRecord, networkRecords) {
-    // If we never requested a document, there's no interstitial error, let other cases handle it.
-    if (!mainRecord) return undefined;
-
-    const interstitialRequest = networkRecords
-      .find(record => record.documentURL.startsWith('chrome-error://'));
-    // If the page didn't end up on a chrome interstitial, there's no error here.
-    if (!interstitialRequest) return undefined;
-
-    // If the main document didn't fail, we didn't end up on an interstitial.
-    // FIXME: This doesn't handle client-side redirects.
-    // None of our error-handling deals with this case either because passContext.url doesn't handle non-network redirects.
-    if (!mainRecord.failed) return undefined;
-
-    // If a request failed with the `net::ERR_CERT_*` collection of errors, then it's a security issue.
-    if (mainRecord.localizedFailDescription.startsWith('net::ERR_CERT')) {
-      return new LHError(LHError.errors.INSECURE_DOCUMENT_REQUEST, {securityMessages:
-        mainRecord.localizedFailDescription});
-    }
-
-    // If we made it this far, it's a generic Chrome interstitial error.
-    return new LHError(LHError.errors.CHROME_INTERSTITIAL_ERROR);
-  }
-
-  /**
-   * Returns an error if we try to load a non-HTML page.
-   * Expects a network request with all redirects resolved, otherwise the MIME type may be incorrect.
-   * @param {LH.Artifacts.NetworkRequest|undefined} finalRecord
-   * @return {LH.LighthouseError|undefined}
-   */
-  static getNonHtmlError(finalRecord) {
-    // MIME types are case-insenstive but Chrome normalizes MIME types to be lowercase.
-    const HTML_MIME_TYPE = 'text/html';
-
-    // If we never requested a document, there's no doctype error, let other cases handle it.
-    if (!finalRecord) return undefined;
-
-    // mimeType is determined by the browser, we assume Chrome is determining mimeType correctly,
-    // independently of 'Content-Type' response headers, and always sending mimeType if well-formed.
-    if (HTML_MIME_TYPE !== finalRecord.mimeType) {
-      return new LHError(LHError.errors.NOT_HTML, {mimeType: finalRecord.mimeType});
-    }
-    return undefined;
-  }
-
-  /**
-   * Returns an error if the page load should be considered failed, e.g. from a
-   * main document request failure, a security issue, etc.
-   * @param {LH.Gatherer.PassContext} passContext
-   * @param {LH.Gatherer.LoadData} loadData
-   * @param {LH.LighthouseError|undefined} navigationError
-   * @return {LH.LighthouseError|undefined}
-   */
-  static getPageLoadError(passContext, loadData, navigationError) {
-    const {networkRecords} = loadData;
-    /** @type {LH.Artifacts.NetworkRequest|undefined} */
-    let mainRecord;
-    try {
-      mainRecord = NetworkAnalyzer.findMainDocument(networkRecords, passContext.url);
-    } catch (_) {}
-
-    // MIME Type is only set on the final redirected document request. Use this for the HTML check instead of root.
-    let finalRecord;
-    if (mainRecord) {
-      finalRecord = NetworkAnalyzer.resolveRedirects(mainRecord);
-    }
-
-    const networkError = GatherRunner.getNetworkError(mainRecord);
-    const interstitialError = GatherRunner.getInterstitialError(mainRecord, networkRecords);
-    const nonHtmlError = GatherRunner.getNonHtmlError(finalRecord);
-
-    // Check to see if we need to ignore the page load failure.
-    // e.g. When the driver is offline, the load will fail without page offline support.
-    if (passContext.passConfig.loadFailureMode === 'ignore') return;
-
-    // We want to special-case the interstitial beyond FAILED_DOCUMENT_REQUEST. See https://github.com/GoogleChrome/lighthouse/pull/8865#issuecomment-497507618
-    if (interstitialError) return interstitialError;
-
-    // Network errors are usually the most specific and provide the best reason for why the page failed to load.
-    // Prefer networkError over navigationError.
-    // Example: `DNS_FAILURE` is better than `NO_FCP`.
-    if (networkError) return networkError;
-
-    // Error if page is not HTML.
-    if (nonHtmlError) return nonHtmlError;
-
-    // Navigation errors are rather generic and express some failure of the page to render properly.
-    // Use `navigationError` as the last resort.
-    // Example: `NO_FCP`, the page never painted content for some unknown reason.
-    return navigationError;
   }
 
   /**
@@ -734,7 +603,7 @@ class GatherRunner {
   /**
    * Starting from about:blank, load the page and run gatherers for this pass.
    * @param {LH.Gatherer.PassContext} passContext
-   * @return {Promise<{artifacts: Partial<LH.GathererArtifacts>, pageLoadError?: LHError}>}
+   * @return {Promise<{artifacts: Partial<LH.GathererArtifacts>, pageLoadError?: LH.LighthouseError}>}
    */
   static async runPass(passContext) {
     const status = {
@@ -769,11 +638,15 @@ class GatherRunner {
     await GatherRunner.pass(passContext, gathererResults);
     const loadData = await GatherRunner.endRecording(passContext);
 
-    // Disable throttling so the afterPass analysis isn't throttled
+    // Disable throttling so the afterPass analysis isn't throttled.
     await emulation.clearThrottling(driver.defaultSession);
 
     // In case of load error, save log and trace with an error prefix, return no artifacts for this pass.
-    const pageLoadError = GatherRunner.getPageLoadError(passContext, loadData, possibleNavError);
+    const pageLoadError = getPageLoadError(possibleNavError, {
+      url: passContext.url,
+      loadFailureMode: passConfig.loadFailureMode,
+      networkRecords: loadData.networkRecords,
+    });
     if (pageLoadError) {
       const localizedMessage = i18n.getFormatted(pageLoadError.friendlyMessage,
           passContext.settings.locale);
