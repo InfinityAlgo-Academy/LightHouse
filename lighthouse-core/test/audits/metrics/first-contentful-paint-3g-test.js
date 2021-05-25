3b3df20cf8e076e94fe67df491a047c2ee4aada0
Brendan Kenny <bckenny@gmail.com>
2021-05-25 13:00:17 -0500

    core: update FCP score curve (#12556)

--- lighthouse-core/test/audits/metrics/first-contentful-paint-3g-test.js
+++ lighthouse-core/test/audits/metrics/first-contentful-paint-3g-test.js
@@ -28,7 +28,7 @@ describe('Performance: first-contentful-paint-3g audit', () => {
     // Use InlineSnapshot here so changes to Lantern coefficients can be easily updated en masse
     expect({score: result.score, value: Math.round(result.numericValue)}).toMatchInlineSnapshot(`
 Object {
-  "score": 0.99,
+  "score": 0.97,
   "value": 2087,
 }
 `);
