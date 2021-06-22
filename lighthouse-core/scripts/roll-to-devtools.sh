f32562dd760c8b45fd5fc6ef5556c6d91dc1d947
Connor Clark <cjamcl@google.com>
2021-06-22 13:23:55 -0700

    tweak

--- lighthouse-core/scripts/roll-to-devtools.sh
+++ lighthouse-core/scripts/roll-to-devtools.sh
@@ -45,9 +45,9 @@ echo -e "$check (Potentially stale) lighthouse-dt-bundle copied."
 
 # copy report code $fe_lh_dir
 fe_lh_report_dir="$fe_lh_dir/report/"
-rsync -avh lighthouse-core/report/html/renderer/ "$fe_lh_report_dir" --exclude="BUILD.gn" --delete
+rsync -avh lighthouse-core/report/html/renderer/ "$fe_lh_report_dir" --exclude="BUILD.gn" --exclude="report-tsconfig.json" --exclude="generated" --exclude="psi.js" --delete
 # file-namer.js is not used, but we should export something so it compiles.
-echo 'export const getFilenamePrefix = () => {};' > "$fe_lh_report_dir/file-namer.js"
+echo 'export const getFilenamePrefix = () => {};' > "$fe_lh_report_dir/common/file-namer.js"
 echo -e "$check Report code copied."
 
 # copy report generator + cached resources into $fe_lh_dir
