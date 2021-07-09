eb0a300d95ba804e01411da3502f694d35eebf7b
Connor Clark <cjamcl@google.com>
2021-07-08 20:23:18 -0700

    wip

--- lighthouse-core/test/repro-test.js
+++ lighthouse-core/test/repro-test.js
@@ -2,9 +2,21 @@
 
 /* eslint-env jest */
 
+const jsdom = require('jsdom');
+
+const url = 'http://www.example.com';
+let dom;
+
 describe('Tests', function() {
   it('pass', async function() {
     const {DOM} = await import('../../report/renderer/dom.js');
     expect(DOM).toBeTruthy();
+
+    const {document, ShadowRoot, Node, HTMLElement} = new jsdom.JSDOM('', {url}).window;
+    global.ShadowRoot = ShadowRoot;
+    global.Node = Node;
+    global.HTMLElement = HTMLElement;
+    global.window = {};
+    dom = new DOM(document);
   });
 });
