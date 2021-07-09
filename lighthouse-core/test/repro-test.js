53751519792c924c49777849ba9322dca577d86d
Connor Clark <cjamcl@google.com>
2021-07-08 20:34:24 -0700

    wip

--- lighthouse-core/test/repro-test.js
+++ lighthouse-core/test/repro-test.js
@@ -9,14 +9,14 @@ let dom;
 
 describe('Tests', function() {
   it('pass', async function() {
-    // const {DOM} = await import('../../report/renderer/dom.js');
-    // expect(DOM).toBeTruthy();
+    const {DOM} = await import('../../report/renderer/dom.js');
+    expect(DOM).toBeTruthy();
 
     const {document, ShadowRoot, Node, HTMLElement} = new jsdom.JSDOM('', {url}).window;
-    global.ShadowRoot = ShadowRoot;
-    global.Node = Node;
-    global.HTMLElement = HTMLElement;
-    global.window = {};
+    // global.ShadowRoot = ShadowRoot;
+    // global.Node = Node;
+    // global.HTMLElement = HTMLElement;
+    // global.window = {};
     // dom = new DOM(document);
   });
 });
