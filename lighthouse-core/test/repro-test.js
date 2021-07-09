48b3fce9d9679d061ca35743f5ce4a2b3aadb392
Connor Clark <cjamcl@google.com>
2021-07-08 20:44:06 -0700

    wip

--- lighthouse-core/test/repro-test.js
+++ lighthouse-core/test/repro-test.js
@@ -6,7 +6,6 @@ require('jsdom');
 
 describe('Tests', function() {
   it('pass', async function() {
-    const {DOM} = await import('../../report/renderer/dom.js');
-    expect(DOM).toBeTruthy();
+    await import('../../report/renderer/dom.js');
   });
 });
