a3c1e75ff888725263e254617e101233960f3577
Connor Clark <cjamcl@google.com>
2021-07-09 13:22:43 -0700

    wip

--- lighthouse-core/test/repro-test.js
+++ lighthouse-core/test/repro-test.js
@@ -3,9 +3,14 @@
 /* eslint-env jest */
 
 // can be any commonjs module
-require('semver');
+// require('semver');
 
 describe('Tests', function() {
+  beforeAll(async () => {
+    const semver = await import('semver');
+    console.log(semver);
+  });
+
   it('pass', async function() {
     await import('../../report/renderer/dom.js');
   });
