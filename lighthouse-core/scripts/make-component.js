4d1f12cb7fbb17ffe6292b590f2a8d2cd268eb6b
Connor Clark <cjamcl@google.com>
2021-07-15 16:54:14 -0700

    load templates file

--- lighthouse-core/scripts/make-component.js
+++ lighthouse-core/scripts/make-component.js
@@ -10,192 +10,12 @@
 const fs = require('fs');
 const jsdom = require('jsdom');
 const {LH_ROOT} = require('../../root.js');
-const { serializeArguments } = require('../gather/driver/execution-context.js');
+const {serializeArguments} = require('../gather/driver/execution-context.js');
 
 // TODO: should change templates.html to use `div` instead of `template`.
 // idea: have paremeters in template?
-// fs.readFileSync(LH_ROOT + '/report/assets/templates.html', 'utf-8').replaceAll('template', 'div');
-const html = `
-<div id="tmpl-lh-metric">
-  <div class="lh-metric" id="${'id'}">
-    <div class="lh-metric__innerwrap">
-      <span class="lh-metric__title"></span>
-      <div class="lh-metric__value">hi</div>
-      <div class="lh-metric__description"></div>
-    </div>
-  </div>
-</div>
-
-<div id="tmpl-lh-topbar">
-<style>
-.lh-topbar {
-  position: sticky;
-  top: 0;
-  left: 0;
-  right: 0;
-  z-index: 1000;
-  display: flex;
-  align-items: center;
-  height: var(--topbar-height);
-  background-color: var(--topbar-background-color);
-  padding: var(--topbar-padding);
-}
-
-.lh-topbar__logo {
-  width: var(--topbar-logo-size);
-  height: var(--topbar-logo-size);
-  user-select: none;
-  flex: none;
-}
-.lh-topbar__logo .shape {
-  fill: var(--report-text-color);
-}
-
-.lh-topbar__url {
-  margin: var(--topbar-padding);
-  text-decoration: none;
-  color: var(--report-text-color);
-  text-overflow: ellipsis;
-  overflow: hidden;
-  white-space: nowrap;
-}
-
-.lh-tools {
-  margin-left: auto;
-  will-change: transform;
-  min-width: var(--report-icon-size);
-}
-.lh-tools__button {
-  width: var(--report-icon-size);
-  height: var(--report-icon-size);
-  cursor: pointer;
-  margin-right: 5px;
-  /* This is actually a button element, but we want to style it like a transparent div. */
-  display: flex;
-  background: none;
-  color: inherit;
-  border: none;
-  padding: 0;
-  font: inherit;
-  outline: inherit;
-}
-.lh-tools__button svg {
-  fill: var(--tools-icon-color);
-}
-.dark .lh-tools__button svg {
-  filter: invert(1);
-}
-.lh-tools__button.active + .lh-tools__dropdown {
-  opacity: 1;
-  clip: rect(-1px, 194px, 242px, -3px);
-  visibility: visible;
-}
-.lh-tools__dropdown {
-  position: absolute;
-  background-color: var(--report-background-color);
-  border: 1px solid var(--report-border-color);
-  border-radius: 3px;
-  padding: calc(var(--default-padding) / 2) 0;
-  cursor: pointer;
-  top: 36px;
-  right: 0;
-  box-shadow: 1px 1px 3px #ccc;
-  min-width: 125px;
-  clip: rect(0, 164px, 0, 0);
-  visibility: hidden;
-  opacity: 0;
-  transition: all 200ms cubic-bezier(0,0,0.2,1);
-}
-.lh-tools__dropdown a {
-  color: currentColor;
-  text-decoration: none;
-  white-space: nowrap;
-  padding: 0 12px;
-  line-height: 2;
-}
-.lh-tools__dropdown a:hover,
-.lh-tools__dropdown a:focus {
-  background-color: var(--color-gray-200);
-  outline: none;
-}
-/* save-gist option hidden in report. */
-.lh-tools__dropdown a[data-action='save-gist'] {
-  display: none;
-}
-
-@media screen and (max-width: 964px) {
-  .lh-tools__dropdown {
-    right: 0;
-    left: initial;
-  }
-}
-@media print {
-  .lh-topbar {
-    position: static;
-    margin-left: 0;
-  }
-
-  .lh-tools__dropdown {
-    display: none;
-  }
-}
-</style>
-
-  <div class="lh-topbar">
-    <!-- Lighthouse logo.  -->
-    <svg class="lh-topbar__logo" viewBox="0 0 24 24">
-      <defs>
-        <linearGradient x1="57.456%" y1="13.086%" x2="18.259%" y2="72.322%" id="lh-topbar__logo--a">
-          <stop stop-color="#262626" stop-opacity=".1" offset="0%"/>
-          <stop stop-color="#262626" stop-opacity="0" offset="100%"/>
-        </linearGradient>
-        <linearGradient x1="100%" y1="50%" x2="0%" y2="50%" id="lh-topbar__logo--b">
-          <stop stop-color="#262626" stop-opacity=".1" offset="0%"/>
-          <stop stop-color="#262626" stop-opacity="0" offset="100%"/>
-        </linearGradient>
-        <linearGradient x1="58.764%" y1="65.756%" x2="36.939%" y2="50.14%" id="lh-topbar__logo--c">
-          <stop stop-color="#262626" stop-opacity=".1" offset="0%"/>
-          <stop stop-color="#262626" stop-opacity="0" offset="100%"/>
-        </linearGradient>
-        <linearGradient x1="41.635%" y1="20.358%" x2="72.863%" y2="85.424%" id="lh-topbar__logo--d">
-          <stop stop-color="#FFF" stop-opacity=".1" offset="0%"/>
-          <stop stop-color="#FFF" stop-opacity="0" offset="100%"/>
-        </linearGradient>
-      </defs>
-      <g fill="none" fill-rule="evenodd">
-        <path d="M12 3l4.125 2.625v3.75H18v2.25h-1.688l1.5 9.375H6.188l1.5-9.375H6v-2.25h1.875V5.648L12 3zm2.201 9.938L9.54 14.633 9 18.028l5.625-2.062-.424-3.028zM12.005 5.67l-1.88 1.207v2.498h3.75V6.86l-1.87-1.19z" fill="#F44B21"/>
-        <path fill="#FFF" d="M14.201 12.938L9.54 14.633 9 18.028l5.625-2.062z"/>
-        <path d="M6 18c-2.042 0-3.95-.01-5.813 0l1.5-9.375h4.326L6 18z" fill="url(#lh-topbar__logo--a)" fill-rule="nonzero" transform="translate(6 3)"/>
-        <path fill="#FFF176" fill-rule="nonzero" d="M13.875 9.375v-2.56l-1.87-1.19-1.88 1.207v2.543z"/>
-        <path fill="url(#lh-topbar__logo--b)" fill-rule="nonzero" d="M0 6.375h6v2.25H0z" transform="translate(6 3)"/>
-        <path fill="url(#lh-topbar__logo--c)" fill-rule="nonzero" d="M6 6.375H1.875v-3.75L6 0z" transform="translate(6 3)"/>
-        <path fill="url(#lh-topbar__logo--d)" fill-rule="nonzero" d="M6 0l4.125 2.625v3.75H12v2.25h-1.688l1.5 9.375H.188l1.5-9.375H0v-2.25h1.875V2.648z" transform="translate(6 3)"/>
-      </g>
-    </svg>
-
-    <a href="" class="lh-topbar__url" target="_blank" rel="noopener"></a>
-
-    <div class="lh-tools">
-      <button id="lh-tools-button" class="lh-tools__button" title="Tools menu" aria-label="Toggle report tools menu" aria-haspopup="menu" aria-expanded="false" aria-controls="lh-tools-dropdown">
-        <svg width="100%" height="100%" viewBox="0 0 24 24">
-            <path d="M0 0h24v24H0z" fill="none"/>
-            <path d="M12 8c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"/>
-        </svg>
-      </button>
-      <div id="lh-tools-dropdown" role="menu" class="lh-tools__dropdown" aria-labelledby="lh-tools-button">
-        <a role="menuitem" tabindex="-1" href="#" class="report-icon report-icon--print" data-i18n="dropdownPrintSummary" data-action="print-summary"></a>
-        <a role="menuitem" tabindex="-1" href="#" class="report-icon report-icon--print" data-i18n="dropdownPrintExpanded" data-action="print-expanded"></a>
-        <a role="menuitem" tabindex="-1" href="#" class="report-icon report-icon--copy" data-i18n="dropdownCopyJSON" data-action="copy"></a>
-        <a role="menuitem" tabindex="-1" href="#" class="report-icon report-icon--download" data-i18n="dropdownSaveHTML" data-action="save-html"></a>
-        <a role="menuitem" tabindex="-1" href="#" class="report-icon report-icon--download" data-i18n="dropdownSaveJSON" data-action="save-json"></a>
-        <a role="menuitem" tabindex="-1" href="#" class="report-icon report-icon--open" data-i18n="dropdownViewer" data-action="open-viewer"></a>
-        <a role="menuitem" tabindex="-1" href="#" class="report-icon report-icon--open" data-i18n="dropdownSaveGist" data-action="save-gist"></a>
-        <a role="menuitem" tabindex="-1" href="#" class="report-icon report-icon--dark" data-i18n="dropdownDarkTheme" data-action="toggle-dark"></a>
-      </div>
-    </div>
-  </div>
-</div>
-`;
+const html = fs.readFileSync(LH_ROOT + '/report/assets/templates.html', 'utf-8')
+  .replace(/template/g, 'div');
 
 const {window} = new jsdom.JSDOM(html);
 
@@ -267,7 +87,8 @@ function compileTemplate(tmpEl) {
   process(tmpEl);
   lines.push(`return ${makeOrGetVarName(tmpEl)};`);
 
-  const componentName = tmpEl.id.replace('tmpl-lh-', '');
+  // TODO: use more parseable names for template id
+  const componentName = tmpEl.id.replace('tmpl-lh-', '').replace(/-/g, '_');
   const functionName = `create${upperFirst(componentName)}Component`;
   const functionCode = createFunctionCode(functionName, lines);
   return {componentName, functionName, functionCode};
