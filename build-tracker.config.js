61abc320f5f1fccc5f86b03ed06f0265f6e09ed3
Paul Irish <paulirish@google.com>
2021-08-17 09:46:25 -0700

    markdown thing from docs

--- build-tracker.config.js
+++ build-tracker.config.js
@@ -2,8 +2,12 @@
 
 'use strict';
 
+const Comparator = require('@build-tracker/comparator').default;
+const last = (xs) => xs[xs.length - 1];
+
+const applicationUrl = 'https://lh-build-tracker.herokuapp.com';
 module.exports = {
-  applicationUrl: 'https://lh-build-tracker.herokuapp.com',
+  applicationUrl,
   // Budgets for these are expressed in https://github.com/paulirish/lh-build-tracker/blob/master/build-tracker.config.js
   artifacts: [
     'dist/lightrider/lighthouse-lr-bundle.js',
@@ -15,9 +19,33 @@ module.exports = {
     'dist/dt-report-resources/report.js',
     'dist/dt-report-resources/report-generator.js',
   ],
-  // onCompare: (data) => {
-  //   // send markdown response somewhere…
-  //   GithubApi.postComment(data.summary.join('\n'));
-  //   return Promise.resolve();
-  // },
+  onCompare: async (data) => {
+    const {comparatorData, summary} = data;
+    // Reconstruct a comparator from the serialized data
+    const comparator = Comparator.deserialize(comparatorData);
+
+    const build = last(comparator.builds);
+
+    const table = comparator.toMarkdown({artifactFilter});
+    const revisions = `${build.getMetaValue('parentRevision')}/${build.getMetaValue('revision')}`;
+    const output = `${summary.join('\n')}
+
+${table}
+
+See the full comparison at [${applicationUrl}/builds/${revisions}](${applicationUrl}/builds/${revisions})`;
+
+    // Post the constructed markdown as a comment
+    console.log(output);
+  },
+};
+
+
+// Filter out any rows from the markdown table that are not failing or did not have a hash change
+const artifactFilter = (row) => {
+  return row.some((cell) => {
+    if (cell.type === 'delta') {
+      return cell.failingBudgets.length > 0 || cell.hashChanged;
+    }
+    return false;
+  });
 };
