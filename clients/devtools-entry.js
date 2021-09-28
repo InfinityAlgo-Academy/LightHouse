e735971b35e1c845d340e863af784ddadf256341
Paul Irish <paulirish@google.com>
2021-09-28 11:23:51 -0700

    comment out direct invocation

--- clients/devtools-entry.js
+++ clients/devtools-entry.js
@@ -153,38 +153,38 @@ if (typeof self !== 'undefined') {
 }
 
 
-// If invoked as CLI, we're gonna read latest-run's trace and analyze that (as desktop)
-if (require.main === module) {
-  localDev = true;
+// // If invoked as CLI, we're gonna read latest-run's trace and analyze that (as desktop)
+// if (require.main === module) {
+//   localDev = true;
 
-  /** @type {LH.Trace} */
-  const trace = JSON.parse(
-    // Gather with:
-    //     lighthouse https://paulirish.com --preset=desktop --only-categories=performance -GA --throttling-method=devtools
-    require('fs').readFileSync(__dirname + '/../latest-run/defaultPass.trace.json', 'utf8')
-  );
+//   /** @type {LH.Trace} */
+//   const trace = JSON.parse(
+//     // Gather with:
+//     //     lighthouse https://paulirish.com --preset=desktop --only-categories=performance -GA --throttling-method=devtools
+//     require('fs').readFileSync(__dirname + '/../latest-run/defaultPass.trace.json', 'utf8')
+//   );
 
-  /**
-   * @param {LH.Trace} trace
-   */
-  const getInitialUrl = trace => {
-    const urls = trace.traceEvents
-    .filter(e =>
-        (e.name === 'navigationStart' && e?.args?.data?.isLoadingMainFrame === true) ||
-        e.name === 'NavigationBodyLoader::StartLoadingBody'
-    )
-    .map(e => e.args.data?.documentLoaderURL || e.args.url);
-    // find most common item: https://stackoverflow.com/a/20762713/89484
-    return urls.sort(
-      (a, b) => urls.filter(v => v === a).length - urls.filter(v => v === b).length).pop();
-  };
+//   /**
+//    * @param {LH.Trace} trace
+//    */
+//   const getInitialUrl = trace => {
+//     const urls = trace.traceEvents
+//     .filter(e =>
+//         (e.name === 'navigationStart' && e?.args?.data?.isLoadingMainFrame === true) ||
+//         e.name === 'NavigationBodyLoader::StartLoadingBody'
+//     )
+//     .map(e => e.args.data?.documentLoaderURL || e.args.url);
+//     // find most common item: https://stackoverflow.com/a/20762713/89484
+//     return urls.sort(
+//       (a, b) => urls.filter(v => v === a).length - urls.filter(v => v === b).length).pop();
+//   };
 
 
-  analyzeTrace(trace, {
-    device: 'desktop',
-    url: getInitialUrl(trace),
-  }).then(res => {
-    require('fs').writeFileSync('./tracereport.html', res?.report[0], 'utf8');
-    console.log('done. written to ./tracereport.html');
-  });
-}
+//   analyzeTrace(trace, {
+//     device: 'desktop',
+//     url: getInitialUrl(trace),
+//   }).then(res => {
+//     require('fs').writeFileSync('./tracereport.html', res?.report[0], 'utf8');
+//     console.log('done. written to ./tracereport.html');
+//   });
+// }
