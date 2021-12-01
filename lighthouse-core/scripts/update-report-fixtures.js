394f6fd537f709b89784f308bc14684b0e986fb6
Paul Irish <paulirish@google.com>
2021-11-30 16:38:01 -0800

    misc(sample_json): make the fixture requestIds deterministic

--- lighthouse-core/scripts/update-report-fixtures.js
+++ lighthouse-core/scripts/update-report-fixtures.js
@@ -5,14 +5,13 @@
  */
 'use strict';
 
-import fs from 'fs';
-
 import * as cli from '../../lighthouse-cli/run.js';
 import * as cliFlags from '../../lighthouse-cli/cli-flags.js';
 import assetSaver from '../lib/asset-saver.js';
 import {server} from '../../lighthouse-cli/test/fixtures/static-server.js';
 import budgetedConfig from '../test/results/sample-config.js';
-import {LH_ROOT} from '../../root.js';
+import {Util} from '../../report/renderer/util.js';
+import walkObject from '../lib/sd-validation/helpers/walk-object.js';
 
 const artifactPath = 'lighthouse-core/test/results/artifacts';
 // All artifacts must have resources from a consistent port, to ensure reproducibility.
@@ -20,10 +19,15 @@ const artifactPath = 'lighthouse-core/test/results/artifacts';
 const MAGIC_SERVER_PORT = 10200;
 
 /**
- * Update the report artifacts. If artifactName is set only that artifact will be updated.
- * @param {keyof LH.Artifacts=} artifactName
+ * Update the report artifacts. If artifactNames is set only those artifacts will be updated.
+ *
+ * Possible usage:
+ *     yarn sample-artifacts OptimizedImages
+ *     yarn sample-artifacts ScriptElements JsUsage MainDocumentContent ResponseCompression
+ *
+ * @param {Array<keyof LH.Artifacts>} artifactNames
  */
-async function update(artifactName) {
+async function update(artifactNames) {
   await server.listen(MAGIC_SERVER_PORT, 'localhost');
 
   const oldArtifacts = assetSaver.loadArtifacts(artifactPath);
@@ -37,24 +41,33 @@ async function update(artifactName) {
   await cli.runLighthouse(url, flags, budgetedConfig);
   await server.close();
 
-  if (artifactName) {
-    // Revert everything except the one artifact
-    const newArtifacts = assetSaver.loadArtifacts(artifactPath);
-    if (!(artifactName in newArtifacts) && !(artifactName in oldArtifacts)) {
-      throw Error('Unknown artifact name: ' + artifactName);
+  const rebaselinedArtifacts = assetSaver.loadArtifacts(artifactPath);
+  const finalArtifacts = oldArtifacts;
+
+  // Revert everything except the specified artifacts
+  if (artifactNames.length) {
+    for (const artifactName of artifactNames) {
+      if (!(artifactName in rebaselinedArtifacts) && !(artifactName in oldArtifacts)) {
+        throw Error('Unknown artifact name: ' + artifactNames);
+      }
+      const newArtifact = rebaselinedArtifacts[artifactName];
+      // @ts-expect-error tsc can't yet express that artifactName is only a single type in each iteration, not a union of types.
+      finalArtifacts[artifactName] = newArtifact;
     }
-    const finalArtifacts = oldArtifacts;
-    const newArtifact = newArtifacts[artifactName];
-    // @ts-expect-error tsc can't yet express that artifactName is only a single type in each iteration, not a union of types.
-    finalArtifacts[artifactName] = newArtifact;
-    await assetSaver.saveArtifacts(finalArtifacts, artifactPath);
+  } else {
+    console.warn('This will overwrite ALL artifacts!');
   }
 
-  // Normalize some data.
-  const artifactsFile = `${LH_ROOT}/${artifactPath}/artifacts.json`;
-  /** @type {LH.Artifacts} */
-  const artifacts = JSON.parse(fs.readFileSync(artifactsFile, 'utf-8'));
+  normalize(finalArtifacts);
 
+  await assetSaver.saveArtifacts(finalArtifacts, artifactPath);
+}
+
+/**
+ * @param {LH.Artifacts} artifacts
+ */
+function normalize(artifacts) {
+  // Reset timing
   for (const timing of artifacts.Timing) {
     // @ts-expect-error: Value actually is writeable at this point.
     timing.startTime = 0;
@@ -62,7 +75,45 @@ async function update(artifactName) {
     timing.duration = 1;
   }
 
-  fs.writeFileSync(artifactsFile, JSON.stringify(artifacts, null, 2));
+  applyDeterministicRequestIds(artifacts);
 }
 
-update(/** @type {keyof LH.Artifacts | undefined} */ (process.argv[2]));
+/**
+ * @param {LH.Artifacts} artifacts
+ */
+function applyDeterministicRequestIds(artifacts) {
+  const oldRequestIdToNew = new Map();
+  let requestCounter = 1;
+
+  // First, devtools logs to slurp up all the requestWillBeSent combos of requestId + URL
+  // Then, the rest of the artifacts, including devtoolsLogs (again). dtl will need a second pass.
+  const objectWalkingPasses = [
+    artifacts.devtoolsLogs,
+    artifacts,
+  ];
+  walkObject(objectWalkingPasses, (name, value, path, obj) => {
+    if (name !== 'requestId') return;
+    if ([...oldRequestIdToNew.values()].includes(value)) return; // already adjusted
+
+    // We need a new happy deterministic id
+    if (![...oldRequestIdToNew.keys()].includes(value)) {
+      const possibleAttachedUrl = obj?.request?.url;
+      // If this isn't defined, we're not dealing with the requestWillBeSent yet.
+      // Oftentimes, requestWillBeSentExtraInfo comes first (out of order, yes). But no problem, we'll address these in the second walk pass.
+      if (!possibleAttachedUrl) return;
+
+      const count = (requestCounter++).toString().padStart(3, '0');
+      const friendlyUrl = Util.parseURL(possibleAttachedUrl).file;
+      oldRequestIdToNew.set(value, `${count}-${friendlyUrl}`);
+    }
+
+    // Apply the requestId
+    if ([...oldRequestIdToNew.keys()].includes(value)) {
+      obj.requestId = oldRequestIdToNew.get(value);
+    } else {
+      throw new Error('Unexpected case. Unknown requestId');
+    }
+  });
+}
+
+update(/** @type {Array<keyof LH.Artifacts>} */ (process.argv.slice(2)));
