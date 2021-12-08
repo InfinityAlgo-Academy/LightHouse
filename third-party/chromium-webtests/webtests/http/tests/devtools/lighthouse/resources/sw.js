0d0a12eaae533fa964fdcfd92249662547527e4b
Connor Clark <cjamcl@google.com>
2021-12-08 14:52:07 -0800

    update

--- third-party/chromium-webtests/webtests/http/tests/devtools/lighthouse/resources/sw.js
+++ third-party/chromium-webtests/webtests/http/tests/devtools/lighthouse/resources/sw.js
@@ -1,13 +1,12 @@
-// Here comes the install event!
-// This only happens once, when the browser sees this
-// version of the ServiceWorker for the first time.
+/*
+ * Copyright 2021 The Chromium Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
 self.addEventListener('install', function(event) {
-  // We pass a promise to event.waitUntil to signal how 
-  // long install takes, and if it failed
   event.waitUntil(
-    // We open a cache…
     caches.open('simple-sw-v1').then(function(cache) {
-      // And add resources to it
       return cache.addAll([
         './',
         'lighthouse-basic.css',
@@ -16,20 +15,9 @@ self.addEventListener('install', function(event) {
   );
 });
 
-// The fetch event happens for the page request with the
-// ServiceWorker's scope, and any request made within that
-// page
 self.addEventListener('fetch', function(event) {
-  // Calling event.respondWith means we're in charge
-  // of providing the response. We pass in a promise
-  // that resolves with a response object
   event.respondWith(
-    // First we look for something in the caches that
-    // matches the request
     caches.match(event.request).then(function(response) {
-      // If we get something, we return it, otherwise
-      // it's null, and we'll pass the request to
-      // fetch, which will use the network.
       return response || fetch(event.request);
     })
   );
