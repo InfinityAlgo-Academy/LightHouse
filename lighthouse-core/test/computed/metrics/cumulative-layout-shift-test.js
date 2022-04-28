6c559d532046b6402e63edd219f00b97f3b27f95
Brendan Kenny <bckenny@gmail.com>
2022-04-28 16:42:14 -0500

    tests: update create-test-trace utility (#13942)

--- lighthouse-core/test/computed/metrics/cumulative-layout-shift-test.js
+++ lighthouse-core/test/computed/metrics/cumulative-layout-shift-test.js
@@ -298,7 +298,7 @@ describe('Metrics: CLS', () => {
 
       it('ignores layout shift data from other tabs', async () => {
         const trace = createTestTrace({timeOrigin: 0, traceEnd: 2000});
-        const mainFrame = trace.traceEvents[0].args.frame;
+        const mainFrame = trace.traceEvents.find(e => e.name === 'navigationStart').args.frame;
         const childFrame = 'CHILDFRAME';
         const otherMainFrame = 'ANOTHERTABOPEN';
         const cat = 'loading,rail,devtools.timeline';
