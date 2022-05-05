9aa283770fe34cb36b83fe26b206f26edf140d46
Connor Clark <cjamcl@google.com>
2022-05-05 09:29:12 -0700

    convert build

--- build/build-treemap.js
+++ build/build-treemap.js
@@ -3,12 +3,12 @@
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
  */
-'use strict';
 
-const fs = require('fs');
-const GhPagesApp = require('./gh-pages-app.js');
-const {LH_ROOT} = require('../root.js');
-const {getIcuMessageIdParts} = require('../shared/localization/format.js');
+import fs from 'fs';
+
+import {GhPagesApp} from './gh-pages-app.js';
+import {LH_ROOT} from '../root.js';
+import {getIcuMessageIdParts} from '../shared/localization/format.js';
 
 /**
  * Extract only the strings needed for treemap into
