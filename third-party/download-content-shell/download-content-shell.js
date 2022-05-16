9249e61a44062bd8b3cd5b381ee84c396b9ce7a2
Connor Clark <cjamcl@google.com>
2022-05-16 13:41:34 -0700

    convert download content shell script

--- third-party/download-content-shell/download-content-shell.js
+++ third-party/download-content-shell/download-content-shell.js
@@ -7,11 +7,12 @@
 
 // Grabbed from https://github.com/ChromeDevTools/devtools-frontend/commit/26bb5ad91b147e9c918819711542ec2337d6b268
 
-const fs = require('fs');
-const path = require('path');
-const shell = require('child_process').execSync;
-const utils = require('./utils.js');
-const {LH_ROOT} = require('../../root.js');
+import fs from 'fs';
+
+import path from 'path';
+import { execSync as shell } from 'child_process';
+import * as utils from './utils.js';
+import { LH_ROOT } from '../../root.js';
 
 const TARGET = 'Release';
 const CONTENT_SHELL_ZIP = 'content-shell.zip';
