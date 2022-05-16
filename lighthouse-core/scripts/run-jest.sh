44c7a83ea5b109161166c3e77ac0d9d634654cb1
Connor Clark <cjamcl@google.com>
2022-05-16 13:11:21 -0700

    keep retrying

--- lighthouse-core/scripts/run-jest.sh
+++ lighthouse-core/scripts/run-jest.sh
@@ -13,26 +13,35 @@
 # See https://github.com/facebook/jest/issues/11438#issuecomment-923835189
 # and https://bugs.chromium.org/p/v8/issues/detail?id=10284
 
+set -o pipefail
+
 DIRNAME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 LH_ROOT="$DIRNAME/../.."
 
-# Copy jest stderr to file, while keeping the output in the terminal and not messing up carriage returns.
-# https://unix.stackexchange.com/a/333204
-# https://superuser.com/a/1124144
-exec 3>&1
-node --experimental-vm-modules ./node_modules/jest/bin/jest.js $* 2>&1 >&3 | tee >(sed 's/.*\r//' >.tmp/jest-stderr.txt)
-jest_exit=$?
-if [ $jest_exit -eq 0 ];
-then
-  exit 0
-fi
+EXTRA_FLAGS=()
 
-if grep -Fq "Test environment has been torn down" .tmp/jest-stderr.txt
-then
-  echo "====================================================="
-  echo "Noticed a v8 bug, so re-running just the failed tests"
-  echo "====================================================="
-  node --experimental-vm-modules ./node_modules/jest/bin/jest.js -f $*
-else
-  exit $jest_exit
-fi
+# Repeating once is typically enough, but just in case...
+count=5
+for i in $(seq $count); do
+  # Copy jest stderr to file, while keeping the output in the terminal and not messing up carriage returns.
+  # https://unix.stackexchange.com/a/333204
+  # https://superuser.com/a/1124144
+  exec 3>&1
+  node --experimental-vm-modules ./node_modules/jest/bin/jest.js ${EXTRA_FLAGS[*]} $* 2>&1 >&3 | tee >(sed 's/.*\r//' >.tmp/jest-stderr.txt)
+  jest_status=$?
+  echo "$jest_status"
+  if [ $jest_status -eq 0 ];
+  then
+    exit 0
+  fi
+
+  if grep -Fq "Test environment has been torn down" .tmp/jest-stderr.txt
+  then
+    echo "====================================================="
+    echo "Noticed a v8 bug, so re-running just the failed tests"
+    echo "====================================================="
+    EXTRA_FLAGS=(-f)
+  else
+    exit $jest_status
+  fi
+done
