243ffede8d19c12c65f79e8acc65d061663f7438
Adam Raine <6752989+adamraine@users.noreply.github.com>
2022-05-27 19:11:43 -0700

    tests(devtools): sync web tests (#14061)

--- third-party/chromium-webtests/webtests/http/tests/devtools/lighthouse/lighthouse-clear-data-warning.js
+++ third-party/chromium-webtests/webtests/http/tests/devtools/lighthouse/lighthouse-clear-data-warning.js
@@ -14,9 +14,10 @@
   });
 
   const containerElement = LighthouseTestRunner.getContainerElement();
-  const checkboxes = containerElement.querySelectorAll('.checkbox');
+  const ensureDisabledNames = ['Accessibility', 'Best practices', 'SEO', 'Progressive Web App'];
+  const checkboxes = Array.from(containerElement.querySelectorAll('.checkbox'));
   for (const checkbox of checkboxes) {
-    if (checkbox.textElement.textContent === 'Performance' || checkbox.textElement.textContent === 'Clear storage') {
+    if (!ensureDisabledNames.includes(checkbox.textElement.textContent)) {
       continue;
     }
 
