e5db1f6e826372e4e609e012b791d173a769a4cb
Connor Clark <cjamcl@google.com>
2022-07-15 15:44:46 -0700

    prepare-test.js

--- lighthouse-core/test/gather/driver/prepare-test.js
+++ lighthouse-core/test/gather/driver/prepare-test.js
@@ -4,29 +4,15 @@
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
  */
 
-import * as td from 'testdouble';
-
 import {createMockSession, createMockDriver} from '../../fraggle-rock/gather/mock-driver.js';
-import {flushAllTimersAndMicrotasks, fnAny, timers} from '../../test-utils.js';
-// import prepare from '../../../gather/driver/prepare.js';
+import {flushAllTimersAndMicrotasks, timers} from '../../test-utils.js';
+import * as prepare from '../../../gather/driver/prepare.js';
 import * as constants from '../../../config/constants.js';
 
-// Some imports needs to be done dynamically, so that their dependencies will be mocked.
-// See: https://jestjs.io/docs/ecmascript-modules#differences-between-esm-and-commonjs
-//      https://github.com/facebook/jest/issues/10025
-/** @type {import('../../../gather/driver/prepare.js')} */
-let prepare;
-
-before(async () => {
-  prepare = (await import('../../../gather/driver/prepare.js'));
-});
-
-const storageMock = {
-  clearDataForOrigin: fnAny(),
-  clearBrowserCaches: fnAny(),
-  getImportantStorageWarning: fnAny(),
-};
-await td.replaceEsm('../../../gather/driver/storage.js', storageMock);
+/** @type {import('./prepare-test.mocks.js').TestContext} */
+// @ts-expect-error
+const testContext = global.lighthouseTestContext;
+const {storageMock} = testContext;
 
 const url = 'https://example.com';
 let sessionMock = createMockSession();
