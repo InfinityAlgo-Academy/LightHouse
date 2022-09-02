164ad231fde6ca7407223f166d318a95b17ee9b1
Connor Clark <cjamcl@google.com>
2022-09-02 12:55:30 -0700

    misc: remove transitionary ES module package.json files (#14348)

--- core/test/fixtures/config/lighthouse-plugin-config-helper/plugin-config-helper.js
+++ core/test/fixtures/config/lighthouse-plugin-config-helper/plugin-config-helper.js
@@ -4,7 +4,6 @@
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
  */
 
-
 /** @type {LH.Config.Plugin} */
 module.exports = {
   groups: {
@@ -23,3 +22,5 @@ module.exports = {
     ],
   },
 };
+
+throw new Error('This file is never actually used. If that changes, just delete this line.');
