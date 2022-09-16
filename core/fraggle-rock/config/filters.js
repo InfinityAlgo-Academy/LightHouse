0ce84c0feb444964dd8fe1555230e1efd785993d
Adam Raine <6752989+adamraine@users.noreply.github.com>
2022-09-16 12:04:03 -0700

    core(fr): always run NetworkUserAgent gatherer (#14392)

--- core/fraggle-rock/config/filters.js
+++ core/fraggle-rock/config/filters.js
@@ -31,7 +31,8 @@ const filterResistantAuditIds = ['full-page-screenshot'];
 
 // Some artifacts are used by the report for additional information.
 // Always run these artifacts even if audits do not request them.
-const filterResistantArtifactIds = ['HostUserAgent', 'HostFormFactor', 'Stacks', 'GatherContext'];
+// These are similar to base artifacts but they cannot be run in all 3 modes.
+const filterResistantArtifactIds = ['Stacks', 'NetworkUserAgent'];
 
 /**
  * Returns the set of audit IDs used in the list of categories.
