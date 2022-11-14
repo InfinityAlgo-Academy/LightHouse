01573f924203bf11b5979100273efe210a3d7084
Adam Raine <ad.st.raine@gmail.com>
2022-11-14 13:46:04 -0800

    fix

--- types/env/lookup-closest-locale/index.d.ts
+++ types/env/lookup-closest-locale/index.d.ts
@@ -4,7 +4,7 @@
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
  */
 
-import {Locale} from '../../lhr/settings';
+type Locale = import('../../lhr/settings.js').Locale;
 
 declare module 'lookup-closest-locale' {
   function lookupClosestLocale(locale: string[]|string|undefined, available: Record<Locale, any>): Locale|undefined;
