7ade015ad99d631314fdc475097abdb18719d0df
Alex N. Jose <alexnj@google.com>
2022-11-14 19:02:18 -0800

    Indent groups and children via CSS.

--- report/renderer/details-renderer.js
+++ report/renderer/details-renderer.js
@@ -456,11 +456,14 @@ export class DetailsRenderer {
         for (const item of details.items.filter((item) => item.entity === group.entity)) {
           aggregateFragment.append(this._renderTableRowsFromItem(item, details.headings));
         }
-        for (const rowEl of this._dom.findAll('tr', aggregateFragment)) {
+        const renderedRows = this._dom.findAll('tr', aggregateFragment)
+        for (const rowEl of renderedRows) {
           // For zebra styling.
           rowEl.classList.add(even ? 'lh-row--even' : 'lh-row--odd');
           if (typeof(group.entity) === 'string') rowEl.dataset.entity = group.entity;
         }
+        // Mark group item.
+        renderedRows[0]?.classList.add('lh-row--group');
         even = !even;
         tbodyElem.append(aggregateFragment);
       }
