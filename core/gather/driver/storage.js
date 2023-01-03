80cdafb0a62f156875d67fb8db50bbcec407ae91
Adam Raine <6752989+adamraine@users.noreply.github.com>
2023-01-03 14:37:33 -0800

    core: warn when clear storage times out (#14476)

--- core/gather/driver/storage.js
+++ core/gather/driver/storage.js
@@ -8,6 +8,7 @@ import log from 'lighthouse-logger';
 
 import * as i18n from '../../lib/i18n/i18n.js';
 
+/* eslint-disable max-len */
 const UIStrings = {
   /**
    * @description A warning that previously-saved data may have affected the measured performance and instructions on how to avoid the problem. "locations" will be a list of possible types of data storage locations, e.g. "IndexedDB",  "Local Storage", or "Web SQL".
@@ -22,7 +23,12 @@ const UIStrings = {
       `Audit this page in an incognito window to prevent those resources ` +
       `from affecting your scores.}
   }`,
+  /** A warning that the data in the browser cache may have affected the measured performance because the operation to clear the browser cache timed out. */
+  warningCacheTimeout: 'Clearing the browser cache timed out. Try auditing this page again and file a bug if the issue persists.',
+  /** A warning that the data on the page's origin may have affected the measured performance because the operation to clear the origin data timed out. */
+  warningOriginDataTimeout: 'Clearing the origin data timed out. Try auditing this page again and file a bug if the issue persists.',
 };
+/* eslint-enable max-len */
 
 const str_ = i18n.createIcuMessageFn(import.meta.url, UIStrings);
 
@@ -30,11 +36,13 @@ const str_ = i18n.createIcuMessageFn(import.meta.url, UIStrings);
 /**
  * @param {LH.Gatherer.FRProtocolSession} session
  * @param {string} url
- * @return {Promise<void>}
+ * @return {Promise<LH.IcuMessage[]>}
  */
 async function clearDataForOrigin(session, url) {
   const status = {msg: 'Cleaning origin data', id: 'lh:storage:clearDataForOrigin'};
   log.time(status);
+
+  const warnings = [];
 
   const origin = new URL(url).origin;
 
@@ -62,12 +70,15 @@ async function clearDataForOrigin(session, url) {
   } catch (err) {
     if (/** @type {LH.LighthouseError} */ (err).code === 'PROTOCOL_TIMEOUT') {
       log.warn('Driver', 'clearDataForOrigin timed out');
+      warnings.push(str_(UIStrings.warningOriginDataTimeout));
     } else {
       throw err;
     }
   } finally {
     log.timeEnd(status);
   }
+
+  return warnings;
 }
 
 /**
@@ -102,19 +113,32 @@ async function getImportantStorageWarning(session, url) {
 /**
  * Clear the network cache on disk and in memory.
  * @param {LH.Gatherer.FRProtocolSession} session
- * @return {Promise<void>}
+ * @return {Promise<LH.IcuMessage[]>}
  */
 async function clearBrowserCaches(session) {
   const status = {msg: 'Cleaning browser cache', id: 'lh:storage:clearBrowserCaches'};
   log.time(status);
 
-  // Wipe entire disk cache
-  await session.sendCommand('Network.clearBrowserCache');
-  // Toggle 'Disable Cache' to evict the memory cache
-  await session.sendCommand('Network.setCacheDisabled', {cacheDisabled: true});
-  await session.sendCommand('Network.setCacheDisabled', {cacheDisabled: false});
+  const warnings = [];
 
-  log.timeEnd(status);
+  try {
+    // Wipe entire disk cache
+    await session.sendCommand('Network.clearBrowserCache');
+    // Toggle 'Disable Cache' to evict the memory cache
+    await session.sendCommand('Network.setCacheDisabled', {cacheDisabled: true});
+    await session.sendCommand('Network.setCacheDisabled', {cacheDisabled: false});
+  } catch (err) {
+    if (/** @type {LH.LighthouseError} */ (err).code === 'PROTOCOL_TIMEOUT') {
+      log.warn('Driver', 'clearBrowserCaches timed out');
+      warnings.push(str_(UIStrings.warningCacheTimeout));
+    } else {
+      throw err;
+    }
+  } finally {
+    log.timeEnd(status);
+  }
+
+  return warnings;
 }
 
 export {
