d7477a7382b249566bc103727d77160f72fdc7d9
Adam Raine <ad.st.raine@gmail.com>
2023-01-10 11:19:57 -0800

    import all

--- types/puppeteer.d.ts
+++ types/puppeteer.d.ts
@@ -4,7 +4,7 @@
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
  */
 
-import puppeteerCore from 'puppeteer-core';
+import * as puppeteerCore from 'puppeteer-core';
 
 /**
  * @fileoverview Lighthouse should be compatible with puppeteer and puppeteer-core even though the types can be slightly different between the two packages.
